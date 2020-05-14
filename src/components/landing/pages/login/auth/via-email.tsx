0352ff7f862f83c00453caf53d63947e6d1fecc2
Philip Molares <philip.molares@udo.edu>
2020-05-14 18:22:17 +0200

    polished via-email component added more i18n
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/components/landing/pages/login/auth/via-email.tsx
+++ src/components/landing/pages/login/auth/via-email.tsx
@@ -6,7 +6,7 @@ import {setUser} from "../../../../../redux/user/actions";
 import {LoginStatus} from "../../../../../redux/user/types";
 
 const ViaEMail: React.FC = () => {
-    useTranslation();
+    const {t} = useTranslation();
     const dispatch = useDispatch();
     const login = () => {
         dispatch(setUser({photo: "https://robohash.org/testy.png", name: "Test", status: LoginStatus.ok}));
@@ -16,18 +16,20 @@ const ViaEMail: React.FC = () => {
             <h5 className="center">
                 <Trans i18nKey="signInVia" values={{service: "E-Mail"}}/>
             </h5>
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Control type="email" size="sm" placeholder="E-Mail" />
+            <Form onSubmit={login}>
+                <Form.Group controlId="email">
+                    <Form.Control isInvalid={error} type="email" size="sm" placeholder={t("email")} />
+                    {feedback}
                 </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Control type="password" size="sm" placeholder="Password" />
+                <Form.Group controlId="password">
+                    <Form.Control isInvalid={error} type="password" size="sm" placeholder={t("password")} />
+                    {feedback}
                 </Form.Group>
                 <Button
+                    type="submit"
                     size="sm"
                     variant="primary"
-                    onClick={login}
                 >
                     <Trans i18nKey="signIn"/>
                 </Button>
