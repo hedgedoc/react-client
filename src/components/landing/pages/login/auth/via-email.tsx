c2b26d04706e8486bd01eb6b1ec466bfd352ae53
Philip Molares <philip.molares@udo.edu>
2020-05-14 18:25:47 +0200

    extracted getAndSetUser to use in via-email
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/components/landing/pages/login/auth/via-email.tsx
+++ src/components/landing/pages/login/auth/via-email.tsx
@@ -1,16 +1,31 @@
 import {Trans, useTranslation} from "react-i18next";
 import {Button, Form} from "react-bootstrap";
-import React, { Fragment } from "react";
+import React, {Fragment, useState} from "react";
+import {postEmailLogin} from "../../../../../api/user";
 import {useDispatch} from "react-redux";
-import {setUser} from "../../../../../redux/user/actions";
-import {LoginStatus} from "../../../../../redux/user/types";
+import {getAndSetUser} from "../../../../initialize/initialize-user-state-from-api";
 
 const ViaEMail: React.FC = () => {
     const {t} = useTranslation();
     const dispatch = useDispatch();
-    const login = () => {
-        dispatch(setUser({photo: "https://robohash.org/testy.png", name: "Test", status: LoginStatus.ok}));
+    const [error, setError] = useState(false);
+    const login = (event: any) => {
+        const email = event.currentTarget[0].value
+        const password = event.currentTarget[1].value
+        postEmailLogin(email, password).then(response => {
+            if (response.status !== 200) {
+                setError(true);
+            } else {
+                // ToDo Extract auth data and set
+                console.log(response)
+                getAndSetUser(dispatch);
+            }
+        })
+        event.preventDefault();
     }
+
+    const feedback = (<Form.Control.Feedback type="invalid"><Trans i18nKey="errorEmailLogin"/></Form.Control.Feedback>);
+
     return (
         <Fragment>
             <h5 className="center">
