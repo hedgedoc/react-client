e32f6410d21c08fcbcc57b7f37295cb8d2889900
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-05-15 22:26:07 +0200

    extract code
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
 import * as serviceWorker from './service-worker';
 import {Landing} from "./components/landing/layout";
-import {setUpFontAwesome} from "./initializers/fontAwesome";
 import {ApplicationLoader} from "./components/loader/application-loader";
 import {Provider} from "react-redux";
-import {setUpI18n} from "./initializers/i18n";
 import {store} from "./utils/store";
-import {loadAllConfig} from "./initializers/configLoader";
+import {setUp} from "./initializers";
 
-setUpFontAwesome();
-const initTasks: (Promise<any>[]) = [setUpI18n(), loadAllConfig(), new Promise(resolve => setTimeout(resolve, 3000))]
+const initTasks = setUp();
 
 ReactDOM.render(
     <Provider store={store}>
