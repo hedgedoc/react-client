a384f93c3dd317eddc97e7c57845a4244b5fb436
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-05-15 23:03:17 +0200

    reduce code
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/initializers/configLoader.ts
+++ src/initializers/configLoader.ts
@@ -16,26 +16,7 @@ export function loadAllConfig() {
             if (!backendConfig) {
                 return Promise.reject("Backend config invalid");
             }
-            setBackendConfig({
-                allowAnonymous: backendConfig.allowAnonymous,
-                authProviders: {
-                    facebook: backendConfig.authProviders.facebook,
-                    github: backendConfig.authProviders.github,
-                    twitter: backendConfig.authProviders.twitter,
-                    gitlab: backendConfig.authProviders.gitlab,
-                    dropbox: backendConfig.authProviders.dropbox,
-                    ldap: backendConfig.authProviders.ldap,
-                    google: backendConfig.authProviders.google,
-                    saml: backendConfig.authProviders.saml,
-                    oauth2: backendConfig.authProviders.oauth2,
-                    email: backendConfig.authProviders.email
-                },
-                specialLinks: {
-                    privacy: backendConfig.specialLinks.privacy,
-                    termsOfUse: backendConfig.specialLinks.termsOfUse,
-                    imprint: backendConfig.specialLinks.imprint,
-                }
-            })
+            setBackendConfig(backendConfig)
         }).then(() => {
             getAndSetUser();
         })
