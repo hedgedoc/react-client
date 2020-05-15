d76161ce0dd4d8b7190e4fe08dd9ee50f92e3393
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-05-15 19:28:19 +0200

    Fix url
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,7 +2,7 @@ import {FrontendConfigState} from "../redux/frontend-config/types";
 import {BackendConfigState} from "../redux/backend-config/types";
 
 export const getBackendConfig = async (backendUrl: string) => {
-    return fetch(backendUrl + '/backend-config')
+    return fetch(backendUrl + '/backend-config.json')
         .then((response) => {
             if (response.status === 200) {
                 return (response.json() as Promise<BackendConfigState>);
@@ -11,7 +11,7 @@ export const getBackendConfig = async (backendUrl: string) => {
 }
 
 export const getFrontendConfig = async () => {
-    return fetch('/config')
+    return fetch('/config.json')
         .then((response) => {
             if (response.status === 200) {
                 return (response.json() as Promise<FrontendConfigState>);
