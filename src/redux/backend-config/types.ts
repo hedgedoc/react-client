4355eaff3de92cf6d7e40c664486e7c98cc49111
mrdrogdrog <mr.drogdrog@gmail.com>
2020-05-15 23:06:31 +0200

    Update src/redux/backend-config/types.ts
    Co-authored-by: Philip Molares <git@molar.es>

--- src/redux/backend-config/types.ts
+++ src/redux/backend-config/types.ts
@@ -1,6 +1,6 @@
 import {Action} from "redux";
 
-export const SET_BACKEND_CONFIG_ACTION_TYPE = 'frontend-config/set';
+export const SET_BACKEND_CONFIG_ACTION_TYPE = 'backend-config/set';
 
 export interface BackendConfigState {
     allowAnonymous: boolean,
