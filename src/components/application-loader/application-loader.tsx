38cc52afec3c7acd767b91362cbcaa3e0012786a
mrdrogdrog <mr.drogdrog@gmail.com>
2020-05-15 23:06:37 +0200

    Update src/components/application-loader/application-loader.tsx
    Co-authored-by: Philip Molares <git@molar.es>

--- src/components/application-loader/application-loader.tsx
+++ src/components/application-loader/application-loader.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import "./application-loader.scss";
 import {Alert} from "react-bootstrap";
