d2ad44cfcc1fc59493f8f5fe76319b3540ddeeea
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-05-16 16:06:42 +0200

    Refresh table view when translation was changed
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/landing/pages/history/history-table/history-table-row.tsx
+++ src/components/landing/pages/history/history-table/history-table-row.tsx
@@ -3,12 +3,14 @@ import {HistoryEntry} from "../history";
 import {PinButton} from "../common/pin-button";
 import {CloseButton} from "../common/close-button";
 import moment from "moment";
+import {useTranslation} from "react-i18next";
 
 interface HistoryTableProp {
     entry: HistoryEntry
 }
 
 export const HistoryTableRow: React.FC<HistoryTableProp> = ({entry}) => {
+    useTranslation()
     return (
         <tr>
             <td>{entry.title}</td>
