3b6aadb3b13de0d4a0b3c78429848e9ef65c093d
Philip Molares <philip.molares@udo.edu>
2020-05-24 23:12:51 +0200

    removed className `inner`
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/components/landing/pages/intro/feature-links.tsx
+++ src/components/landing/pages/intro/feature-links.tsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 const FeatureLinks: React.FC = () => {
     return (
         <Row className="mb-5">
-            <Col md={4} className="inner">
+            <Col md={4}>
                 <Link to={"/features#Share-Notes"} className="text-light">
                     <FontAwesomeIcon icon="bolt" size="3x"/>
                     <h5>
@@ -15,7 +15,7 @@ const FeatureLinks: React.FC = () => {
                     </h5>
                 </Link>
             </Col>
-            <Col md={4} className="inner">
+            <Col md={4}>
                 <Link to={"/features#MathJax"} className="text-light">
                     <FontAwesomeIcon icon="chart-bar" size="3x"/>
                     <h5>
@@ -23,7 +23,7 @@ const FeatureLinks: React.FC = () => {
                     </h5>
                 </Link>
             </Col>
-            <Col md={4} className="inner">
+            <Col md={4}>
                 <Link to={"/features#Slide-Mode"} className="text-light">
                     <FontAwesomeIcon icon="tv" size="3x"/>
                     <h5>
