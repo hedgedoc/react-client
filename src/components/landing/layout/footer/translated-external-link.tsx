6e35147110a28a159a96fd3f75aae8f07206eb0d
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-05-28 18:17:52 +0200

    Organize imports
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/landing/layout/footer/translated-external-link.tsx
+++ src/components/landing/layout/footer/translated-external-link.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Trans, useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import { ExternalLink } from './external-link'
 
 export interface TranslatedLinkProps {
