0d8b6e55f8c6d26bdce85907984ed66f1863728b
Philip Molares <philip.molares@udo.edu>
2020-05-29 22:11:56 +0200

    started work on codemirror integration
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/components/editor/editor-window/editor-window.tsx
+++ src/components/editor/editor-window/editor-window.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Controlled as CodeMirror } from 'react-codemirror2'
+import './editor-window.scss'
 
 const EditorWindow: React.FC = () => {
+  const [content, setContent] = useState<string>('It\'s very easy to make some words **bold** and other words *italic* with Markdown.\nYou can even [link to Google!](http://google.com)')
   return (
-    <div style={{ backgroundColor: 'green' }}>
-            Hello, EditorWindow!
-    </div>
+    <CodeMirror
+      className="h-100 w-100"
+      value={content}
+      options={{
+        mode: 'markdown',
+        lineNumbers: true
+      }}
+      onBeforeChange={(editor, data, value) => {
+        setContent(value)
+      }}
+      onChange={(editor, data, value) => {
+        console.log('change!')
+      }}
+    />
   )
 }
 
