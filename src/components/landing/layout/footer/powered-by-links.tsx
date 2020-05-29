5c8c61bdbfbdb56e4aba11a00adfcbb0bb84add2
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-05-29 09:14:58 +0200

    Move external link and translated-external-link
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/landing/layout/footer/powered-by-links.tsx
+++ src/components/landing/layout/footer/powered-by-links.tsx
@@ -2,8 +2,8 @@ import React, { Fragment } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
 import { ApplicationState } from '../../../../redux'
-import { ExternalLink } from './external-link'
-import { TranslatedExternalLink } from './translated-external-link'
+import { ExternalLink } from '../../../links/external-link'
+import { TranslatedExternalLink } from '../../../links/translated-external-link'
 import { VersionInfo } from '../version-info/version-info'
 
 export const PoweredByLinks: React.FC = () => {
@@ -25,10 +25,7 @@ export const PoweredByLinks: React.FC = () => {
         Object.entries({ ...defaultLinks, ...(config.specialLinks) }).map(([i18nKey, href]) =>
           <Fragment key={i18nKey}>
             &nbsp;|&nbsp;
-            <TranslatedExternalLink
-              href={href}
-              i18nKey={i18nKey}
-            />
+            <TranslatedExternalLink href={href} i18nKey={i18nKey}/>
           </Fragment>
         )
       }
--- src/components/links/translated-external-link.tsx
+++ src/components/links/translated-external-link.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { IconProp } from '../../utils/iconProp'
 import { ExternalLink } from './external-link'
 
 export interface TranslatedLinkProps {
-  href: string;
   i18nKey: string;
+  href: string;
+  icon?: IconProp;
+  className?: string
 }
 
-const TranslatedExternalLink: React.FC<TranslatedLinkProps> = ({ href, i18nKey }) => {
+const TranslatedExternalLink: React.FC<TranslatedLinkProps> = ({ i18nKey, ...props }) => {
   const { t } = useTranslation()
   return (
-    <ExternalLink href={href} text={t(i18nKey)}/>
+    <ExternalLink text={t(i18nKey)} {...props}/>
   )
 }
 
