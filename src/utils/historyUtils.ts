4f24c41ddcb0a7b5a6b860e2a387b22e49381ddf
Philip Molares <philip.molares@udo.edu>
2020-06-03 14:30:13 +0200

    extracted convertOldHistory function
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/utils/historyUtils.ts
+++ src/utils/historyUtils.ts
@@ -1,5 +1,5 @@
-import { HistoryEntry } from '../components/landing/pages/history/history'
 import moment from 'moment'
+import { HistoryEntry } from '../components/landing/pages/history/history'
 import { HistoryToolbarState } from '../components/landing/pages/history/history-toolbar/history-toolbar'
 import { SortModeEnum } from '../components/sort-button/sort-button'
 
@@ -59,29 +59,34 @@ export function formatHistoryDate (date: Date): string {
 }
 
 export interface OldHistoryEntry {
-    id: string;
-    text: string;
-    time: number;
-    tags: string[];
-    pinned: boolean;
+  id: string;
+  text: string;
+  time: number;
+  tags: string[];
+  pinned: boolean;
+}
+
+export function convertOldHistory (oldHistory: OldHistoryEntry[]): HistoryEntry[] {
+  return oldHistory.map((entry: OldHistoryEntry) => {
+    return {
+      id: entry.id,
+      title: entry.text,
+      lastVisited: moment(entry.time).toDate(),
+      tags: entry.tags,
+      pinned: entry.pinned
+    }
+  })
 }
 
 export function loadHistoryFromLocalStore (): HistoryEntry[] {
   const historyJsonString = window.localStorage.getItem('history')
+
   if (!historyJsonString) {
     // if localStorage["history"] is empty we check the old localStorage["notehistory"]
     // and convert it to the new format
     const oldHistoryJsonString = window.localStorage.getItem('notehistory')
     const oldHistory = oldHistoryJsonString ? JSON.parse(JSON.parse(oldHistoryJsonString)) as OldHistoryEntry[] : []
-    return oldHistory.map((entry: OldHistoryEntry) => {
-      return {
-        id: entry.id,
-        title: entry.text,
-        lastVisited: moment(entry.time).toDate(),
-        tags: entry.tags,
-        pinned: entry.pinned
-      }
-    })
+    return convertOldHistory(oldHistory)
   } else {
     return JSON.parse(historyJsonString) as HistoryEntry[]
   }
