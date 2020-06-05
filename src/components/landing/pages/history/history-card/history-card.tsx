1d2dc88b1cf3cba6ecbcc075a144219af96dd116
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-06-05 21:03:25 +0200

    rearrage card
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/landing/pages/history/history-card/history-card.tsx
+++ src/components/landing/pages/history/history-card/history-card.tsx
@@ -12,26 +12,28 @@ import './history-card.scss'
 export const HistoryCard: React.FC<HistoryEntryProps> = ({ entry, onPinClick, onSyncClick }) => {
   return (
     <div className="p-2 col-xs-12 col-sm-6 col-md-6 col-lg-4">
-      <Card className="p-0 card-min-height" text={'dark'} bg={'light'}>
-        <Card.Body className="p-2 d-flex flex-column justify-content-between">
-          <div className={'d-flex justify-content-between align-items-start'}>
-            <div className={'d-flex flex-column'}>
-              <PinButton isDark={false} isPinned={entry.pinned} onPinClick={() => onPinClick(entry.id)}/>
-              <SyncStatus isDark={false} location={entry.location} onSync={() => onSyncClick(entry.id)}
-                className={'mt-1'}/>
-            </div>
+      <Card className="card-min-height" text={'dark'} bg={'light'}>
+        <Card.Body className="p-2 d-flex flex-row justify-content-between">
+          <div className={'d-flex flex-column'}>
+            <PinButton isDark={false} isPinned={entry.pinned} onPinClick={() => onPinClick(entry.id)}/>
+            <SyncStatus isDark={false} location={entry.location} onSync={() => onSyncClick(entry.id)}
+              className={'mt-1'}/>
+          </div>
+          <div className={'d-flex flex-column justify-content-between'}>
+            <Card.Title className="m-0 mt-1dot5">{entry.title}</Card.Title>
             <div>
-              <Card.Title className="m-0 mt-1dot5">{entry.title}</Card.Title>
               <div className="text-black-50 mt-2">
                 <ForkAwesomeIcon icon="clock-o"/> {moment(entry.lastVisited).fromNow()}<br/>
                 {formatHistoryDate(entry.lastVisited)}
-                <div>
-                  {
-                    entry.tags.map((tag) => <Badge variant={'dark'} className={'mr-1 mb-1'} key={tag}>{tag}</Badge>)
-                  }
-                </div>
+              </div>
+              <div className={'card-footer-min-height p-0'}>
+                {
+                  entry.tags.map((tag) => <Badge variant={'dark'} className={'mr-1 mb-1'} key={tag}>{tag}</Badge>)
+                }
               </div>
             </div>
+          </div>
+          <div className={'d-flex flex-column'}>
             <CloseButton isDark={false}/>
           </div>
         </Card.Body>
