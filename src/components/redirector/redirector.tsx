7c2356218c9f0e633580bd7f8dc4653f21cdfb9e
Philip Molares <philip.molares@udo.edu>
2020-06-12 23:05:39 +0200

    Redirector component now checks if a note has the preVersionTwoNote flag set and only then redirects it to /n/$note
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/components/redirector/redirector.tsx
+++ src/components/redirector/redirector.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Redirect } from 'react-router'
 import { useParams } from 'react-router-dom'
+import { getNote } from '../../api/note'
+import { NotFound } from '../error/not-found'
 
 interface RouteParameters {
   id: string
@@ -8,7 +10,19 @@ interface RouteParameters {
 
 export const Redirector: React.FC = () => {
   const { id } = useParams<RouteParameters>()
-  return (
-    <Redirect to={`/n/${id}`}/>
-  )
+  const [error, setError] = useState<boolean | null>(null)
+
+  useEffect(() => {
+    getNote(id)
+      .then((noteFromAPI) => setError(noteFromAPI.preVersionTwoNote))
+      .catch(() => setError(true))
+  }, [id])
+
+  if (error) {
+    return (<NotFound/>)
+  } else if (!error && error != null) {
+    return (<Redirect to={`/n/${id}`}/>)
+  } else {
+    return (<span>Loading</span>)
+  }
 }
