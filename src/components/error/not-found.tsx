40b4fba2bb21324bc25aafb67ae8017ac289f3ec
Philip Molares <philip.molares@udo.edu>
2020-06-13 00:10:19 +0200

    added LandingLayout around the NotFound component, so users can easily navigate away from the component
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/components/error/not-found.tsx
+++ src/components/error/not-found.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { LandingLayout } from '../landing/landing-layout'
 
 export const NotFound: React.FC = () => {
   return (
-    <div className='text-white d-flex align-items-center justify-content-center vh-100 vw-100'>
-      <h1>404 Not Found <small>oops.</small></h1>
-    </div>
+    <LandingLayout>
+      <div className='text-white d-flex align-items-center justify-content-center'>
+        <h1>404 Not Found <small>oops.</small></h1>
+      </div>
+    </LandingLayout>
   )
 }
