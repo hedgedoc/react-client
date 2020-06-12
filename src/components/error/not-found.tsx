6cbd3f488229cb17e8eddbd7998fcc4464ff07de
Philip Molares <philip.molares@udo.edu>
2020-06-13 00:22:34 +0200

    added margin in NotFound component
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/components/error/not-found.tsx
+++ src/components/error/not-found.tsx
@@ -4,7 +4,7 @@ import { LandingLayout } from '../landing/landing-layout'
 export const NotFound: React.FC = () => {
   return (
     <LandingLayout>
-      <div className='text-white d-flex align-items-center justify-content-center'>
+      <div className='text-white d-flex align-items-center justify-content-center my-5'>
         <h1>404 Not Found <small>oops.</small></h1>
       </div>
     </LandingLayout>
