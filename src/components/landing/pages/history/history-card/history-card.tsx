8da0f7dcde420197a6bf7be66a72c07483ea3917
Erik Michelson <github@erik.michelson.eu>
2020-06-13 02:47:02 +0200

    Added Link objects around history-cards and the title in table-view
    This commit adds one Link element wrapping the title of a note in the table view and one Link element wrapping the whole card of a note in cards view.
    
    This behaviour currently doesn't work completely correct as other buttons on the card become unclickable and needs to be fixed in another commit before merging this branch.

--- src/components/landing/pages/history/history-card/history-card.tsx
+++ src/components/landing/pages/history/history-card/history-card.tsx
@@ -1,6 +1,7 @@
 import moment from 'moment'
 import React from 'react'
 import { Badge, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import { formatHistoryDate } from '../../../../../utils/historyUtils'
 import { ForkAwesomeIcon } from '../../../../common/fork-awesome/fork-awesome-icon'
 import { EntryMenu } from '../common/entry-menu'
@@ -11,36 +12,38 @@ import './history-card.scss'
 export const HistoryCard: React.FC<HistoryEntryProps> = ({ entry, onPinClick, onRemoveClick }) => {
   return (
     <div className="p-2 col-xs-12 col-sm-6 col-md-6 col-lg-4">
-      <Card className="card-min-height" text={'dark'} bg={'light'}>
-        <Card.Body className="p-2 d-flex flex-row justify-content-between">
-          <div className={'d-flex flex-column'}>
-            <PinButton isDark={false} isPinned={entry.pinned} onPinClick={() => onPinClick(entry.id, entry.location)}/>
-          </div>
-          <div className={'d-flex flex-column justify-content-between'}>
-            <Card.Title className="m-0 mt-1dot5">{entry.title}</Card.Title>
-            <div>
-              <div className="text-black-50 mt-2">
-                <ForkAwesomeIcon icon="clock-o"/> {moment(entry.lastVisited).fromNow()}<br/>
-                {formatHistoryDate(entry.lastVisited)}
-              </div>
-              <div className={'card-footer-min-height p-0'}>
-                {
-                  entry.tags.map((tag) => <Badge variant={'dark'} className={'mr-1 mb-1'} key={tag}>{tag}</Badge>)
-                }
+      <Link to={`/n/${entry.id}`} className="text-decoration-none">
+        <Card className="card-min-height" text={'dark'} bg={'light'}>
+          <Card.Body className="p-2 d-flex flex-row justify-content-between">
+            <div className={'d-flex flex-column'}>
+              <PinButton isDark={false} isPinned={entry.pinned} onPinClick={() => onPinClick(entry.id, entry.location)}/>
+            </div>
+            <div className={'d-flex flex-column justify-content-between'}>
+              <Card.Title className="m-0 mt-1dot5">{entry.title}</Card.Title>
+              <div>
+                <div className="text-black-50 mt-2">
+                  <ForkAwesomeIcon icon="clock-o"/> {moment(entry.lastVisited).fromNow()}<br/>
+                  {formatHistoryDate(entry.lastVisited)}
+                </div>
+                <div className={'card-footer-min-height p-0'}>
+                  {
+                    entry.tags.map((tag) => <Badge variant={'dark'} className={'mr-1 mb-1'} key={tag}>{tag}</Badge>)
+                  }
+                </div>
               </div>
             </div>
-          </div>
-          <div className={'d-flex flex-column'}>
-            <EntryMenu
-              id={entry.id}
-              location={entry.location}
-              isDark={false}
-              onRemove={() => onRemoveClick(entry.id, entry.location)}
-              onDelete={() => onRemoveClick(entry.id, entry.location)}
-            />
-          </div>
-        </Card.Body>
-      </Card>
+            <div className={'d-flex flex-column'}>
+              <EntryMenu
+                id={entry.id}
+                location={entry.location}
+                isDark={false}
+                onRemove={() => onRemoveClick(entry.id, entry.location)}
+                onDelete={() => onRemoveClick(entry.id, entry.location)}
+              />
+            </div>
+          </Card.Body>
+        </Card>
+      </Link>
     </div>
   )
 }
