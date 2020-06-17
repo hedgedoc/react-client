05e1d824d85726100c32bff75f4f5d1acdd4b630
Philip Molares <philip@mauricedoepke.de>
2020-06-17 09:51:26 +0200

    added css rules added old embedding code

--- src/components/editor/markdown-preview/plugins/youtube.ts
+++ src/components/editor/markdown-preview/plugins/youtube.ts
@@ -5,11 +5,19 @@ export const youtubePlugin: RegexOptions = {
   regex: /^(?:(?:http(?:s)?:\/\/)?(?:www.)?(?:youtube\.com\/(?:[^\\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\\/\s]{11}))$/,
   replace: (match) => {
     const thumbnailSrc = `//img.youtube.com/vi/${match}/hqdefault.jpg`
-    return `<img src="${thumbnailSrc}" alt="youtube video"/>`
+    return `<div class="youtube"><img class="youtube" src="${thumbnailSrc}" alt="youtube video"/><i class="youtube icon fa fa-youtube-play fa-5x"/></div>`
   }
 }
 
 /*
+click to replace img with <iframe> embedding
+function W (t: HTMLDivElement, n: string) {
+  if (e(t).attr('data-videoid')) {
+    const r = e("<iframe frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>")
+    e(r).attr('src', n + e(t).attr('data-videoid') + '?autoplay=1'), e(t).find('img').css('visibility', 'hidden'), e(t).append(r)
+  }
+}
+
 MarkdownIt.P = new Plugin(
   // regexp to match
   /{%youtube\s*([\d\D]*?)\s*%}/,
