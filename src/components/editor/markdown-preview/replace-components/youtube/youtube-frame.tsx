ae835b1e0d8905746e644eb713ef6f991f1b0b8b
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-06-20 13:11:54 +0200

    reformat code
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/editor/markdown-preview/replace-components/youtube/youtube-frame.tsx
+++ src/components/editor/markdown-preview/replace-components/youtube/youtube-frame.tsx
@@ -3,7 +3,7 @@ import { ComponentReplacer } from '../../markdown-preview'
 import { OneClickEmbedding } from '../one-click-frame/one-click-embedding'
 import { getIdFromCodiMdTag, VideoFrameProps } from '../video-util'
 
-const getElementReplacement:ComponentReplacer = (node, counterMap) => {
+const getElementReplacement: ComponentReplacer = (node, counterMap) => {
   const videoId = getIdFromCodiMdTag(node, 'youtube')
   if (videoId) {
     const count = (counterMap.get(videoId) || 0) + 1
@@ -14,7 +14,9 @@ const getElementReplacement:ComponentReplacer = (node, counterMap) => {
 
 export const YouTubeFrame: React.FC<VideoFrameProps> = ({ id }) => {
   return (
-    <OneClickEmbedding containerClassName={'embed-responsive embed-responsive-16by9'} previewContainerClassName={'embed-responsive-item'} hoverIcon={'youtube-play'} loadingImageUrl={`//i.ytimg.com/vi/${id}/maxresdefault.jpg`}>
+    <OneClickEmbedding containerClassName={'embed-responsive embed-responsive-16by9'}
+      previewContainerClassName={'embed-responsive-item'} hoverIcon={'youtube-play'}
+      loadingImageUrl={`//i.ytimg.com/vi/${id}/maxresdefault.jpg`}>
       <iframe className='embed-responsive-item' title={`youtube video of ${id}`}
         src={`//www.youtube-nocookie.com/embed/${id}?autoplay=1`}
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"/>
