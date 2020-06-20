4751372088e4f048e06a8dff82648ff456e173c9
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-06-20 21:26:03 +0200

    only highlight languages that are supported
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/editor/markdown-renderer/markdown-it-plugins/highlighted-code.ts
+++ src/components/editor/markdown-renderer/markdown-it-plugins/highlighted-code.ts
@@ -1,9 +1,10 @@
+import hljs from 'highlight.js'
 import MarkdownIt from 'markdown-it/lib'
 
 export const highlightedCode: MarkdownIt.PluginSimple = (md: MarkdownIt) => {
   md.core.ruler.push('highlighted-code', (state) => {
     state.tokens.forEach(token => {
-      if (token.type === 'fence' && token.info !== '') {
+      if (token.type === 'fence' && token.info !== '' && hljs.listLanguages().indexOf(token.info) > -1) {
         token.attrJoin('language', token.info)
       }
     })
