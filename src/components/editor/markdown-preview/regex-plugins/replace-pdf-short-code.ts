26ec2e6d8a33a3eb53b69e38d5b4bbcb60525ff3
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-06-20 13:01:11 +0200

    Fix regex name
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/editor/markdown-preview/regex-plugins/replace-pdf-short-code.ts
+++ src/components/editor/markdown-preview/regex-plugins/replace-pdf-short-code.ts
@@ -1,7 +1,7 @@
 import { RegexOptions } from '../../../../external-types/markdown-it-regex/interface'
 
 export const replacePdfShortCode: RegexOptions = {
-  name: 'legacy-pdf-short-code',
+  name: 'pdf-short-code',
   regex: /^{%pdf (.*) ?%}$/,
   replace: (match) => {
     // ESLint wants to collapse this tag, but then the tag won't be valid html anymore.
