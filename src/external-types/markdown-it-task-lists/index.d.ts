329620bb7c892827e7fc743fcb63b713eb23b422
Erik Michelson <github@erik.michelson.eu>
2020-06-21 02:13:21 +0200

    Added types for options of the task lists markdown-it plugin
    These options could become useful when we implement the render->markdown flow of changing the clicked-state of a checklist item.

--- src/external-types/markdown-it-task-lists/index.d.ts
+++ src/external-types/markdown-it-task-lists/index.d.ts
@@ -1,6 +1,7 @@
 
 declare module 'markdown-it-task-lists' {
   import MarkdownIt from 'markdown-it/lib'
-  const markdownItTaskLists: MarkdownIt.PluginSimple
+  import { TaskListsOptions } from './interface'
+  const markdownItTaskLists: MarkdownIt.PluginWithOptions<TaskListsOptions>
   export = markdownItTaskLists
 }
---
+++ src/external-types/markdown-it-task-lists/interface.ts
@@ -0,0 +1,5 @@
+export interface TaskListsOptions {
+  enabled: boolean
+  label: boolean
+  labelAfter: boolean
+}
