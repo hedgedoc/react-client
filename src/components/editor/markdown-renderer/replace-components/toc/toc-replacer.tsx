6eb141784e9a403eae14c422ada630ce00cb4b9f
Erik Michelson <github@erik.michelson.eu>
2020-06-21 13:43:36 +0200

    Removed unnecessary div wrapper of toc

--- src/components/editor/markdown-renderer/replace-components/toc/toc-replacer.tsx
+++ src/components/editor/markdown-renderer/replace-components/toc/toc-replacer.tsx
@@ -5,8 +5,11 @@ import { SubNodeConverter } from '../../markdown-renderer'
 const getElementReplacement = (node: DomElement, index: number, counterMap: Map<string, number>, nodeConverter: SubNodeConverter): (ReactElement | undefined) => {
   if (node.name === 'p' && node.children && node.children.length === 1) {
     const possibleTocDiv = node.children[0]
-    if (possibleTocDiv.name === 'div' && possibleTocDiv.attribs && possibleTocDiv.attribs.class && possibleTocDiv.attribs.class === 'table-of-contents') {
-      return nodeConverter(possibleTocDiv, index)
+    if (possibleTocDiv.name === 'div' && possibleTocDiv.attribs && possibleTocDiv.attribs.class &&
+      possibleTocDiv.attribs.class === 'table-of-contents' && possibleTocDiv.children && possibleTocDiv.children.length === 1) {
+      const listElement = possibleTocDiv.children[0]
+      listElement.attribs = Object.assign(listElement.attribs || {}, { class: 'table-of-contents' })
+      return nodeConverter(possibleTocDiv.children[0], index)
     }
   }
 }
