070d1f5467f6164f48354ab4fb6e06be93da3803
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-06-25 20:53:01 +0200

    Move markdown toc into own component
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

---
+++ src/components/editor/markdown-toc/markdown-toc.tsx
@@ -0,0 +1,30 @@
+import React, { Fragment, ReactElement } from 'react'
+import { TocAst } from '../../../external-types/markdown-it-toc-done-right/interface'
+import { ShowIf } from '../../common/show-if/show-if'
+import './markdown-toc.scss'
+
+export interface MarkdownTocProps {
+  ast: TocAst
+  maxDepth?: number
+}
+
+const convertLevel = (toc: TocAst): ReactElement => {
+  return (
+    <Fragment>
+      {toc.n}
+      <ShowIf condition={toc.c.length > 0}>
+        <ul>
+          {toc.c.map(convertLevel)}
+        </ul>
+      </ShowIf>
+    </Fragment>
+  )
+}
+
+export const MarkdownToc: React.FC<MarkdownTocProps> = ({ ast, maxDepth = 3 }) => {
+  return (
+    <div className={'markdown-toc'}>
+      {convertLevel(ast)}
+    </div>
+  )
+}
--- src/components/editor/renderer-window/render-window.tsx
+++ src/components/editor/renderer-window/render-window.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react'
 import useResizeObserver from 'use-resize-observer'
 import { TocAst } from '../../../external-types/markdown-it-toc-done-right/interface'
 import { MarkdownRenderer } from '../markdown-renderer/markdown-renderer'
+import { MarkdownToc } from '../markdown-toc/markdown-toc'
 
 interface RenderWindowProps {
     content: string
@@ -13,20 +14,17 @@ export const RenderWindow: React.FC<RenderWindowProps> = ({ content }) => {
   const { width } = useResizeObserver({ ref: renderer })
 
   return (
-    <div className={'bg-light container-fluid flex-fill pb-5 flex-row d-flex'} ref={renderer}>
+    <div className={'bg-light container-fluid flex-fill pb-5 flex-row d-flex min-h-100'} ref={renderer}>
       <div className={'col-md'}/>
       <MarkdownRenderer
         content={content}
         className={'container-fluid'}
         onTocChange={(tocAst) => setTocAst(tocAst)}/>
+
       <div className={'col-md'}>
         {
-          (width || 0) >= 1024 ? (
-            <div className={'markdown-sidebar-scroller'}>
-              <ul>
-                <li>Test</li>
-              </ul>
-            </div>
+          (width || 0) >= 1280 && tocAst ? (
+            <MarkdownToc ast={tocAst}/>
           ) : null
         }
       </div>
