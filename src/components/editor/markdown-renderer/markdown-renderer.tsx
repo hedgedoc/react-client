ebcaf41f97b2733dd0dea3e043974896b286f8cc
Philip Molares <philip.molares@udo.edu>
2020-06-25 19:18:55 +0200

    added image-replacer

--- src/components/editor/markdown-renderer/markdown-renderer.tsx
+++ src/components/editor/markdown-renderer/markdown-renderer.tsx
@@ -38,6 +38,7 @@ import { replaceYouTubeLink } from './regex-plugins/replace-youtube-link'
 import { ComponentReplacer, SubNodeConverter } from './replace-components/ComponentReplacer'
 import { GistReplacer } from './replace-components/gist/gist-replacer'
 import { HighlightedCodeReplacer } from './replace-components/highlighted-fence/highlighted-fence-replacer'
+import { ImageReplacer } from './replace-components/image/image-replacer'
 import { MathjaxReplacer } from './replace-components/mathjax/mathjax-replacer'
 import { PdfReplacer } from './replace-components/pdf/pdf-replacer'
 import { QuoteOptionsReplacer } from './replace-components/quote-options/quote-options-replacer'
@@ -127,7 +128,8 @@ const MarkdownRenderer: React.FC<MarkdownPreviewProps> = ({ content, wide }) => 
       new TocReplacer(),
       new HighlightedCodeReplacer(),
       new QuoteOptionsReplacer(),
-      new MathjaxReplacer()
+      new MathjaxReplacer(),
+      new ImageReplacer()
     ]
     const html: string = markdownIt.render(content)
     const transform: Transform = (node, index) => {
