6afffd41868790e1d222742fa5e5ffc02e13cbcc
Philip Molares <philip.molares@udo.edu>
2020-07-01 17:23:48 +0200

    fix wrapping

--- src/components/editor/editor-window/tool-bar/tool-bar.tsx
+++ src/components/editor/editor-window/tool-bar/tool-bar.tsx
@@ -1,59 +1,62 @@
 import React from 'react'
-import { Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap'
+import { Button, ButtonToolbar } from 'react-bootstrap'
 import { ForkAwesomeIcon } from '../../../common/fork-awesome/fork-awesome-icon'
 import './tool-bar.scss'
 
-export const ToolBar: React.FC = () => {
+export interface ToolBarProps {
+  selection: string,
+  onChange: (selection: string) => void
+}
+
+export const ToolBar: React.FC<ToolBarProps> = ({ selection, onChange }) => {
   const notImplemented = () => {
     alert('This feature is not yet implemented')
   }
 
   return (
     <ButtonToolbar>
-      <ButtonGroup>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="bold"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="italic"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="strikethrough"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="header"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="quote-right"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="list"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="list-ol"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="check-square"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="link"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="file-image-o"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="upload"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="table"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="minus"/>
-        </Button>
-        <Button onClick={notImplemented}>
-          <ForkAwesomeIcon icon="comment"/>
-        </Button>
-      </ButtonGroup>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="bold"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="italic"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="strikethrough"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="header"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="quote-right"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="list"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="list-ol"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="check-square"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="link"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="file-image-o"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="upload"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="table"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="minus"/>
+      </Button>
+      <Button onClick={notImplemented}>
+        <ForkAwesomeIcon icon="comment"/>
+      </Button>
     </ButtonToolbar>
   )
 }
