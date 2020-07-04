f6fb5a1ab40029cf8fe0623113f520dd03a2b925
Erik Michelson <github@erik.michelson.eu>
2020-07-04 02:47:41 +0200

    Replaced connection indicator in editor top bar with user-menu

--- src/components/editor/task-bar/task-bar.tsx
+++ src/components/editor/task-bar/task-bar.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Button, Nav, Navbar } from 'react-bootstrap'
 import { Trans, useTranslation } from 'react-i18next'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { ApplicationState } from '../../../redux'
 import { Branding } from '../../common/branding/branding'
 import { ForkAwesomeIcon } from '../../common/fork-awesome/fork-awesome-icon'
-import { ConnectionIndicator } from './connection-indicator'
+import { ShowIf } from '../../common/show-if/show-if'
+import { SignInButton } from '../../landing/layout/navigation/sign-in-button'
+import { UserDropdown } from '../../landing/layout/navigation/user-dropdown/user-dropdown'
 import { DarkModeButton } from './dark-mode-button'
 import { EditorMenu } from './editor-menu'
 import { EditorViewMode } from './editor-view-mode'
@@ -12,6 +16,8 @@ import { HelpButton } from './help-button'
 
 const TaskBar: React.FC = () => {
   useTranslation()
+  const user = useSelector((state: ApplicationState) => state.user)
+
   return (
     <Navbar bg={'light'}>
       <Nav className="mr-auto d-flex align-items-center">
@@ -30,15 +36,15 @@ const TaskBar: React.FC = () => {
         <Button className="ml-2 text-secondary" size="sm" variant="outline-light">
           <ForkAwesomeIcon icon="plus"/> <Trans i18nKey="editor.menu.new"/>
         </Button>
-        <Button className="ml-2 text-secondary" size="sm" variant="outline-light">
-          <ForkAwesomeIcon icon="share-square-o"/> <Trans i18nKey="editor.menu.publish"/>
-        </Button>
         <div className="text-secondary">
           <EditorMenu/>
         </div>
-        <div className="mr-2">
-          <ConnectionIndicator/>
-        </div>
+        <ShowIf condition={!user}>
+          <SignInButton />
+        </ShowIf>
+        <ShowIf condition={!!user}>
+          <UserDropdown />
+        </ShowIf>
       </Nav>
     </Navbar>
   )
