c5c476aa4930e7bc59ea6763719c38ae1bee775b
Erik Michelson <github@erik.michelson.eu>
2020-07-07 21:33:26 +0200

    Added translated titles to buttons of toolbar

--- src/components/editor/editor-window/tool-bar/tool-bar.tsx
+++ src/components/editor/editor-window/tool-bar/tool-bar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, ButtonToolbar } from 'react-bootstrap'
+import { useTranslation } from 'react-i18next'
 import { ForkAwesomeIcon } from '../../../common/fork-awesome/fork-awesome-icon'
 import { Positions } from '../interfaces'
 import './tool-bar.scss'
@@ -21,6 +22,7 @@ export interface ToolBarProps {
 }
 
 export const ToolBar: React.FC<ToolBarProps> = ({ content, positions, onContentChange }) => {
+  const { t } = useTranslation()
   const notImplemented = () => {
     alert('This feature is not yet implemented')
   }
@@ -39,49 +41,49 @@ export const ToolBar: React.FC<ToolBarProps> = ({ content, positions, onContentC
 
   return (
     <ButtonToolbar className='flex-nowrap bg-light'>
-      <Button variant='light' onClick={makeSelectionBold}>
+      <Button variant='light' onClick={makeSelectionBold} title={t('editor.editorToolbar.bold')}>
         <ForkAwesomeIcon icon="bold"/>
       </Button>
-      <Button variant='light' onClick={makeSelectionItalic}>
+      <Button variant='light' onClick={makeSelectionItalic} title={t('editor.editorToolbar.italic')}>
         <ForkAwesomeIcon icon="italic"/>
       </Button>
-      <Button variant='light' onClick={strikeThroughSelection}>
+      <Button variant='light' onClick={strikeThroughSelection} title={t('editor.editorToolbar.strikethrough')}>
         <ForkAwesomeIcon icon="strikethrough"/>
       </Button>
-      <Button variant='light' onClick={() => addHeaderLevel(content, positions.startPosition, onContentChange)}>
+      <Button variant='light' onClick={() => addHeaderLevel(content, positions.startPosition, onContentChange)} title={t('editor.editorToolbar.header')}>
         <ForkAwesomeIcon icon="header"/>
       </Button>
-      <Button variant='light' onClick={() => addCodeFences(content, positions.startPosition, positions.endPosition, onContentChange)}>
+      <Button variant='light' onClick={() => addCodeFences(content, positions.startPosition, positions.endPosition, onContentChange)} title={t('editor.editorToolbar.code')}>
         <ForkAwesomeIcon icon="code"/>
       </Button>
-      <Button variant='light' onClick={() => addQuotes(content, positions.startPosition, positions.endPosition, onContentChange)}>
+      <Button variant='light' onClick={() => addQuotes(content, positions.startPosition, positions.endPosition, onContentChange)} title={t('editor.editorToolbar.blockquote')}>
         <ForkAwesomeIcon icon="quote-right"/>
       </Button>
-      <Button variant='light' onClick={addList}>
+      <Button variant='light' onClick={addList} title={t('editor.editorToolbar.unorderedList')}>
         <ForkAwesomeIcon icon="list"/>
       </Button>
-      <Button variant='light' onClick={addOrderedList}>
+      <Button variant='light' onClick={addOrderedList} title={t('editor.editorToolbar.orderedList')}>
         <ForkAwesomeIcon icon="list-ol"/>
       </Button>
-      <Button variant='light' onClick={addTaskList}>
+      <Button variant='light' onClick={addTaskList} title={t('editor.editorToolbar.checkList')}>
         <ForkAwesomeIcon icon="check-square"/>
       </Button>
-      <Button variant='light' onClick={() => addLink(content, positions.startPosition, positions.endPosition, onContentChange)}>
+      <Button variant='light' onClick={() => addLink(content, positions.startPosition, positions.endPosition, onContentChange)} title={t('editor.editorToolbar.link')}>
         <ForkAwesomeIcon icon="link"/>
       </Button>
-      <Button variant='light' onClick={() => addImage(content, positions.startPosition, positions.endPosition, onContentChange)}>
+      <Button variant='light' onClick={() => addImage(content, positions.startPosition, positions.endPosition, onContentChange)} title={t('editor.editorToolbar.image')}>
         <ForkAwesomeIcon icon="picture-o"/>
       </Button>
-      <Button variant='light' onClick={notImplemented}>
+      <Button variant='light' onClick={notImplemented} title={t('editor.editorToolbar.uploadImage')}>
         <ForkAwesomeIcon icon="upload"/>
       </Button>
-      <Button variant='light' onClick={addTable}>
+      <Button variant='light' onClick={addTable} title={t('editor.editorToolbar.table')}>
         <ForkAwesomeIcon icon="table"/>
       </Button>
-      <Button variant='light' onClick={addLine}>
+      <Button variant='light' onClick={addLine} title={t('editor.editorToolbar.line')}>
         <ForkAwesomeIcon icon="minus"/>
       </Button>
-      <Button variant='light' onClick={addComment}>
+      <Button variant='light' onClick={addComment} title={t('editor.editorToolbar.comment')}>
         <ForkAwesomeIcon icon="comment"/>
       </Button>
     </ButtonToolbar>
