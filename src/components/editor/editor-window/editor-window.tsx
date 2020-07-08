cb3f1e758e5ffd567ead73082aa23b7b12e95814
Philip Molares <philip.molares@udo.edu>
2020-07-09 00:14:14 +0200

    removed unnecessary continueComment addon

--- src/components/editor/editor-window/editor-window.tsx
+++ src/components/editor/editor-window/editor-window.tsx
@@ -76,31 +76,29 @@ export const EditorWindow: React.FC<EditorWindowProps> = ({ onContentChange, con
           showCursorWhenSelecting: true,
           highlightSelectionMatches: true,
           indentUnit: 4,
-          continueComments: 'Enter',
           inputStyle: 'textarea',
-          matchBrackets: true,
-          autoCloseBrackets: true,
-          matchTags: {
-            bothTags: true
-          },
-          autoCloseTags: true,
-          foldGutter: true,
-          gutters: [
-            'CodeMirror-linenumbers',
-            'authorship-gutters',
-            'CodeMirror-foldgutter'
-          ],
-          extraKeys: defaultKeyMap,
-          flattenSpans: true,
-          addModeClass: true,
-          autoRefresh: true,
-          // otherCursors: true,
-          placeholder: t('editor.placeholder')
-        }}
-        onBeforeChange={(editor, data, value) => {
-          onContentChange(value)
-        }}
-      />
-    </div>
+        matchBrackets: true,
+        autoCloseBrackets: true,
+        matchTags: {
+          bothTags: true
+        },
+        autoCloseTags: true,
+        foldGutter: true,
+        gutters: [
+          'CodeMirror-linenumbers',
+          'authorship-gutters',
+          'CodeMirror-foldgutter'
+        ],
+        extraKeys: defaultKeyMap,
+        flattenSpans: true,
+        addModeClass: true,
+        autoRefresh: true,
+        // otherCursors: true,
+        placeholder: t('editor.placeholder')
+      }}
+      onBeforeChange={(editor, data, value) => {
+        onContentChange(value)
+      }}
+    /></div>
   )
 }
