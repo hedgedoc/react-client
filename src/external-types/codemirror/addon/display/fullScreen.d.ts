8da672f343fb60bfd651c18e6df98de6bcf5d161
Philip Molares <philip.molares@udo.edu>
2020-07-10 01:02:45 +0200

    fix importing errors
    apparently one needs to import codemirror in the fullScreen.d.ts file for typescript to correctly import codemirror
    also the css file needs to be imported in the editor-window

--- src/external-types/codemirror/addon/display/fullScreen.d.ts
+++ src/external-types/codemirror/addon/display/fullScreen.d.ts
@@ -1,16 +1,8 @@
-// Type definitions for codemirror
-// Project: https://github.com/codemirror/CodeMirror
-// Definitions by: ficristo <https://github.com/ficristo>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-// See docs https://codemirror.net/doc/manual.html#addon_placeholder
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import CodeMirror from 'codemirror'
 
 declare module 'codemirror' {
     interface EditorConfiguration {
-        /**
-         * Adds a placeholder option that can be used to make content appear in the editor when it is empty and not focused.
-         * It can hold either a string or a DOM node. Also gives the editor a CodeMirror-empty CSS class whenever it doesn't contain any text.
-         */
         fullScreen?: boolean;
     }
 }
