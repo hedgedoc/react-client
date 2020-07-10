ccb9a446fd0e096ca488104978c0f41b970782f0
Erik Michelson <github@erik.michelson.eu>
2020-07-10 02:39:44 +0200

    Added API call and image url replacing

--- src/components/editor/markdown-renderer/replace-components/image/image-frame.tsx
+++ src/components/editor/markdown-renderer/replace-components/image/image-frame.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { getProxiedUrl } from '../../../../../api/imageProxy'
+import { ApplicationState } from '../../../../../redux'
 
-export const ImageFrame: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = ({alt, ...props}) => {
+export const ImageFrame: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = ({ alt, src, ...props }) => {
+  const [imageUrl, setImageUrl] = useState(src)
+  const imageProxyEnabled = useSelector((state: ApplicationState) => state.backendConfig.imageProxy)
+
+  useEffect(() => {
+    if (!imageProxyEnabled || !imageUrl) {
+      return
+    }
+    const fetchProxiedUrl = async (): Promise<void> => {
+      const proxiedUrlResponse = await getProxiedUrl(imageUrl)
+      setImageUrl(proxiedUrlResponse.src)
+    }
+    fetchProxiedUrl().catch(err => console.error(err))
+  }, [imageUrl, imageProxyEnabled])
+
   return (
-    <img alt={alt} {...props}/>
+    <img alt={alt} src={imageUrl} {...props}/>
   )
 }
