23e8660f5364145110909034415bb62f9de629c5
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-07-16 22:36:45 +0200

    Add more specific rules for finding the input text are in e2e test
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- cypress/integration/documentTitle.spec.ts
+++ cypress/integration/documentTitle.spec.ts
@@ -6,26 +6,26 @@ describe('Document Title', () => {
     cy.visit('/n/test')
     cy.get('.btn.active.btn-outline-secondary > i.fa-columns')
       .should('exist')
-    cy.get('textarea')
+    cy.get('.CodeMirror textarea')
       .type('{ctrl}a', { force: true })
       .type('{backspace}')
   })
 
   describe('title should be yaml metadata title', () => {
     it('just yaml metadata title', () => {
-      cy.get('textarea')
+      cy.get('.CodeMirror textarea')
         .type(`---\ntitle: ${title}\n---`)
       cy.title().should('eq', `${title} - CodiMD @ ${branding.name}`)
     })
 
     it('yaml metadata title and opengraph title', () => {
-      cy.get('textarea')
+      cy.get('.CodeMirror textarea')
         .type(`---\ntitle: ${title}\nopengraph:\n  title: False title\n{backspace}{backspace}---`)
       cy.title().should('eq', `${title} - CodiMD @ ${branding.name}`)
     })
 
     it('yaml metadata title, opengraph title and first heading', () => {
-      cy.get('textarea')
+      cy.get('.CodeMirror textarea')
         .type(`---\ntitle: ${title}\nopengraph:\n  title: False title\n{backspace}{backspace}---\n# a first title`)
       cy.title().should('eq', `${title} - CodiMD @ ${branding.name}`)
     })
@@ -33,13 +33,13 @@ describe('Document Title', () => {
 
   describe('title should be opengraph title', () => {
     it('just opengraph title', () => {
-      cy.get('textarea')
+      cy.get('.CodeMirror textarea')
         .type(`---\nopengraph:\n  title: ${title}\n{backspace}{backspace}---`)
       cy.title().should('eq', `${title} - CodiMD @ ${branding.name}`)
     })
 
     it('opengraph title and first heading', () => {
-      cy.get('textarea')
+      cy.get('.CodeMirror textarea')
         .type(`---\nopengraph:\n  title: ${title}\n{backspace}{backspace}---\n# a first title`)
       cy.title().should('eq', `${title} - CodiMD @ ${branding.name}`)
     })
@@ -47,7 +47,7 @@ describe('Document Title', () => {
 
   describe('title should be first heading', () => {
     it('just first heading', () => {
-      cy.get('textarea')
+      cy.get('.CodeMirror textarea')
         .type(`# ${title}`)
       cy.title().should('eq', `${title} - CodiMD @ ${branding.name}`)
     })
