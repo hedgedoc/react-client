4be59127a3514d54914d1ef6ddd430cb14a220b1
Philip Molares <philip.molares@udo.edu>
2020-07-16 15:28:08 +0200

    added documentTitle e2e test

---
+++ cypress/integration/documentTitle.spec.ts
@@ -0,0 +1,55 @@
+import { branding } from '../support/config'
+
+const title = 'This is a test title'
+describe('Document Title', () => {
+  beforeEach(() => {
+    cy.visit('/n/test')
+    cy.get('.btn.active.btn-outline-secondary > i.fa-columns')
+      .should('exist')
+    cy.get('textarea')
+      .type('{ctrl}a', { force: true })
+      .type('{backspace}')
+  })
+
+  describe('title should be yaml metadata title', () => {
+    it('just yaml metadata title', () => {
+      cy.get('textarea')
+        .type(`---\ntitle: ${title}\n---`)
+      cy.title().should('eq', `${title} - CodiMD @ ${branding.name}`)
+    })
+
+    it('yaml metadata title and opengraph title', () => {
+      cy.get('textarea')
+        .type(`---\ntitle: ${title}\nopengraph:\n  title: False title\n{backspace}{backspace}---`)
+      cy.title().should('eq', `${title} - CodiMD @ ${branding.name}`)
+    })
+
+    it('yaml metadata title, opengraph title and first heading', () => {
+      cy.get('textarea')
+        .type(`---\ntitle: ${title}\nopengraph:\n  title: False title\n{backspace}{backspace}---\n# a first title`)
+      cy.title().should('eq', `${title} - CodiMD @ ${branding.name}`)
+    })
+  })
+
+  describe('title should be opengraph title', () => {
+    it('just opengraph title', () => {
+      cy.get('textarea')
+        .type(`---\nopengraph:\n  title: ${title}\n{backspace}{backspace}---`)
+      cy.title().should('eq', `${title} - CodiMD @ ${branding.name}`)
+    })
+
+    it('opengraph title and first heading', () => {
+      cy.get('textarea')
+        .type(`---\nopengraph:\n  title: ${title}\n{backspace}{backspace}---\n# a first title`)
+      cy.title().should('eq', `${title} - CodiMD @ ${branding.name}`)
+    })
+  })
+
+  describe('title should be first heading', () => {
+    it('just first heading', () => {
+      cy.get('textarea')
+        .type(`# ${title}`)
+      cy.title().should('eq', `${title} - CodiMD @ ${branding.name}`)
+    })
+  })
+})
