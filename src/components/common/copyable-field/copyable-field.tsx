176cf7772e058bc62a2c1b5525b27d0cb2274b08
Erik Michelson <github@erik.michelson.eu>
2020-07-18 01:54:43 +0200

    Made version input field to a common component

--- src/components/common/copyable-field/copyable-field.tsx
+++ src/components/common/copyable-field/copyable-field.tsx
@@ -1,13 +1,13 @@
 import React, { Fragment, useRef, useState } from 'react'
 import { Button, FormControl, InputGroup, Overlay, Tooltip } from 'react-bootstrap'
 import { Trans, useTranslation } from 'react-i18next'
-import { ForkAwesomeIcon } from '../../../common/fork-awesome/fork-awesome-icon'
+import { ForkAwesomeIcon } from '../fork-awesome/fork-awesome-icon'
 
-export interface VersionInputFieldProps {
-  version: string
+export interface CopyableFieldProps {
+  content: string
 }
 
-export const VersionInputField: React.FC<VersionInputFieldProps> = ({ version }) => {
+export const CopyableField: React.FC<CopyableFieldProps> = ({ content }) => {
   useTranslation()
   const inputField = useRef<HTMLInputElement>(null)
   const [showCopiedTooltip, setShowCopiedTooltip] = useState(false)
@@ -21,20 +21,28 @@ export const VersionInputField: React.FC<VersionInputFieldProps> = ({ version })
     })
   }
 
+  const selectContent = () => {
+    if (!inputField.current) {
+      return
+    }
+    inputField.current.focus()
+    inputField.current.setSelectionRange(0, inputField.current.value.length)
+  }
+
   return (
     <Fragment>
       <Overlay target={inputField} show={showCopiedTooltip} placement="top">
         {(props) => (
-          <Tooltip id={'copied_' + version} {...props}>
+          <Tooltip id={'copied_' + content} {...props}>
             <Trans i18nKey={'landing.versionInfo.successfullyCopied'}/>
           </Tooltip>
         )}
       </Overlay>
 
       <InputGroup className="mb-3">
-        <FormControl readOnly={true} ref={inputField} className={'text-center'} value={version} />
+        <FormControl readOnly={true} ref={inputField} className={'text-center'} value={content} onMouseEnter={selectContent} />
         <InputGroup.Append>
-          <Button variant="outline-secondary" onClick={() => copyToClipboard(version)} title={'Copy'}>
+          <Button variant="outline-secondary" onClick={() => copyToClipboard(content)} title={'Copy'}>
             <ForkAwesomeIcon icon='files-o'/>
           </Button>
         </InputGroup.Append>
