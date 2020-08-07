db44c7506cda0e3c15786663d373a545cd63c615
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-08-07 18:03:13 +0200

    Add details to document info
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

---
+++ src/components/editor/document-bar/document-info-line.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ForkAwesomeIcon, IconName } from '../../common/fork-awesome/fork-awesome-icon'
+
+export interface DocumentInfoLineProps {
+  icon: IconName
+}
+
+export const DocumentInfoLine: React.FC<DocumentInfoLineProps> = ({ icon, children }) => {
+  return (
+    <span className={'d-flex align-items-center'}>
+      <ForkAwesomeIcon icon={icon} size={'2x'} fixedWidth={true}/>
+      {children}
+    </span>
+  )
+}
