7752cd0dbe5285bd96c77abc90f1129fd7504ea1
Philip Molares <philip.molares@udo.edu>
2020-08-07 12:11:02 +0200

    moved document bar to the top

--- src/components/editor/document-bar/doc-timestamps.tsx
+++ src/components/editor/document-bar/doc-timestamps.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { ForkAwesomeIcon } from '../../common/fork-awesome/fork-awesome-icon'
 import './doc-timestamps.scss'
-import { UserAvatar } from '../../landing/layout/user-avatar/user-avatar'
 
 export const DocTimestamps: React.FC = () => {
   return (
-    <div className={'d-flex flex-column timestamps'}>
-      <span><ForkAwesomeIcon icon='clock-o' fixedWidth={true}/> an anonymous user edited this note 3 minutes ago</span>
-      <span className='d-inline-flex'><UserAvatar name={'Test'} photo={'https://1.gravatar.com/avatar/767fc9c115a1b989744c755db47feb60?s=200&r=pg&d=mp'}/> created this note 11 minutes ago</span>
+    <div className={'d-flex flex-row align-items-center timestamps'}>
+      <span title='Philip Molares created this note 11 minutes ago'><ForkAwesomeIcon icon='plus-square-o' fixedWidth={true}/> Philip Molares — 11 minutes ago</span>
+      &nbsp;
+      <span title='Tilman Vatteroth edited this note 3 minutes ago'><ForkAwesomeIcon icon='pencil-square-o' fixedWidth={true}/> Tilman Vatteroth — 3 minutes ago</span>
     </div>
   )
 }
