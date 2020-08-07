9919c774f94ca3a829d9d5a373a718b1bd71f554
Erik Michelson <github@erik.michelson.eu>
2020-08-07 23:38:28 +0200

    Added link to presentation mode button

--- src/components/editor/task-bar/task-bar.tsx
+++ src/components/editor/task-bar/task-bar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button, Nav, Navbar } from 'react-bootstrap'
 import { Trans, useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
+import { useParams } from 'react-router'
 import { Link } from 'react-router-dom'
 import { ApplicationState } from '../../../redux'
 import { Branding } from '../../common/branding/branding'
@@ -9,12 +10,14 @@ import { ForkAwesomeIcon } from '../../common/fork-awesome/fork-awesome-icon'
 import { ShowIf } from '../../common/show-if/show-if'
 import { SignInButton } from '../../landing/layout/navigation/sign-in-button'
 import { UserDropdown } from '../../landing/layout/navigation/user-dropdown/user-dropdown'
+import { EditorPathParams } from '../editor'
 import { DarkModeButton } from './dark-mode-button'
 import { EditorViewMode } from './editor-view-mode'
 import { HelpButton } from './help-button'
 
-const TaskBar: React.FC = () => {
+export const TaskBar: React.FC = () => {
   const { t } = useTranslation()
+  const { id } = useParams<EditorPathParams>()
   const user = useSelector((state: ApplicationState) => state.user)
 
   return (
@@ -29,9 +32,11 @@ const TaskBar: React.FC = () => {
         </Navbar.Brand>
         <EditorViewMode/>
         <DarkModeButton/>
-        <Button title={t('editor.menu.slideMode')} className="ml-2 text-secondary" size="sm" variant="outline-light">
-          <ForkAwesomeIcon icon="television"/>
-        </Button>
+        <Link to={`/p/${id}`} target='_blank'>
+          <Button title={t('editor.menu.slideMode')} className="ml-2 text-secondary" size="sm" variant="outline-light">
+            <ForkAwesomeIcon icon="television"/>
+          </Button>
+        </Link>
         <HelpButton/>
       </Nav>
       <Nav className="d-flex align-items-center text-secondary">
@@ -48,5 +53,3 @@ const TaskBar: React.FC = () => {
     </Navbar>
   )
 }
-
-export { TaskBar }
