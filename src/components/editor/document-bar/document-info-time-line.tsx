c50f9f267ea38dd9e2f846bcc204e5eba75f8fb3
Erik Michelson <github@erik.michelson.eu>
2020-08-09 00:20:13 +0200

    Add avatar icon to document-info modal

--- src/components/editor/document-bar/document-info-time-line.tsx
+++ src/components/editor/document-bar/document-info-time-line.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import { Trans, useTranslation } from 'react-i18next'
 import { IconName } from '../../common/fork-awesome/fork-awesome-icon'
 import { DocumentInfoLine } from './document-info-line'
+import './document-info-time-line.scss'
 
 export interface DocumentInfoLineWithTimeProps {
   time: Moment,
   mode: DocumentInfoLineWithTimeMode
   userName: string
+  profileImageSrc: string
 }
 
 export enum DocumentInfoLineWithTimeMode {
@@ -15,7 +17,7 @@ export enum DocumentInfoLineWithTimeMode {
   EDITED
 }
 
-export const DocumentInfoTimeLine: React.FC<DocumentInfoLineWithTimeProps> = ({ time, mode, userName }) => {
+export const DocumentInfoTimeLine: React.FC<DocumentInfoLineWithTimeProps> = ({ time, mode, userName, profileImageSrc }) => {
   useTranslation()
 
   const i18nKey = mode === DocumentInfoLineWithTimeMode.CREATED ? 'editor.menu.created' : 'editor.menu.edited'
@@ -23,6 +25,7 @@ export const DocumentInfoTimeLine: React.FC<DocumentInfoLineWithTimeProps> = ({ 
 
   return (
     <DocumentInfoLine icon={icon}>
+      <img alt={`avatar icon of ${userName}`} src={profileImageSrc} className='mr-2 rounded document-info-avatar'/>
       <b>{ userName }</b>
       &nbsp;
       <i>
