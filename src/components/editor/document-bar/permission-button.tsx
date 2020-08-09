11e027ec5aeaa5e10ee7dbfcf5e85b28ee684064
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-08-09 15:04:18 +0200

    Fix share button and i18n files
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/editor/document-bar/permission-button.tsx
+++ src/components/editor/document-bar/permission-button.tsx
@@ -11,13 +11,13 @@ export const PermissionButton: React.FC = () => {
     <Fragment>
       <Button variant={'light'} className={'mx-1'} size={'sm'} onClick={() => setShowReadOnly(true)}>
         <ForkAwesomeIcon icon={'lock'} className={'mx-1'}/>
-        <Trans i18nKey={'editor.menu.permission'}/>
+        <Trans i18nKey={'editor.menu.permissions'}/>
       </Button>
       <CommonModal
         show={showReadOnly}
         onHide={() => setShowReadOnly(false)}
         closeButton={true}
-        titleI18nKey={'editor.menu.permission'}>
+        titleI18nKey={'editor.modal.permissions.title'}>
         <div>
           <img className={'w-100'} src={'https://thumbs.gfycat.com/ImpassionedDeliriousIndianpalmsquirrel-size_restricted.gif'} alt={'Placeholder'}/>
         </div>
--- src/components/editor/document-bar/share-link-button.tsx
+++ src/components/editor/document-bar/share-link-button.tsx
@@ -5,22 +5,22 @@ import { CopyableField } from '../../common/copyable-field/copyable-field'
 import { ForkAwesomeIcon } from '../../common/fork-awesome/fork-awesome-icon'
 import { CommonModal } from '../../common/modals/common-modal'
 
-export const ShareButton: React.FC = () => {
+export const ShareLinkButton: React.FC = () => {
   const [showReadOnly, setShowReadOnly] = useState(false)
 
   return (
     <Fragment>
       <Button variant={'light'} className={'mx-1'} size={'sm'} onClick={() => setShowReadOnly(true)}>
         <ForkAwesomeIcon icon={'share'} className={'mx-1'}/>
-        <Trans i18nKey={'editor.menu.share'}/>
+        <Trans i18nKey={'editor.menu.shareLink'}/>
       </Button>
       <CommonModal
         show={showReadOnly}
         onHide={() => setShowReadOnly(false)}
         closeButton={true}
-        titleI18nKey={'editor.menu.share'}>
+        titleI18nKey={'editor.modal.shareLink.title'}>
         <div className={'mx-4 d-flex flex-column justify-items-between'}>
-          <span className={'my-4'}><Trans i18nKey={'editor.menu.shareText'}/></span>
+          <span className={'my-4'}><Trans i18nKey={'editor.modal.shareLink.description'}/></span>
           <CopyableField content={'https://example.com'}/>
         </div>
       </CommonModal>
