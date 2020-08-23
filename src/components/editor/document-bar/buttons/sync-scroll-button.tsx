7b0c6e32d7d07bbbc380180bb2e934f5e38ebfee
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-08-23 15:04:07 +0200

    Use toggler
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/editor/document-bar/buttons/sync-scroll-button.tsx
+++ src/components/editor/document-bar/buttons/sync-scroll-button.tsx
@@ -1,18 +1,31 @@
 import React, { useCallback } from 'react'
+import { ToggleButton, ToggleButtonGroup } from 'react-bootstrap'
+import { Trans } from 'react-i18next'
 import { useSelector } from 'react-redux'
 import { ApplicationState } from '../../../../redux'
 import { setEditorSyncScroll } from '../../../../redux/editor/methods'
-import { TranslatedIconButton } from '../../../common/icon-button/translated-icon-button'
+import { ForkAwesomeIcon } from '../../../common/fork-awesome/fork-awesome-icon'
+import { ForkAwesomeStack } from '../../../common/fork-awesome/fork-awesome-stack'
 
 export const SyncScrollButton: React.FC = () => {
   const syncScroll: boolean = useSelector((state: ApplicationState) => state.editorConfig.syncScroll)
   const translation = syncScroll ? 'editor.documentBar.disableSyncScroll' : 'editor.documentBar.enableSyncScroll'
-  const icon = syncScroll ? 'chain-broken' : 'link'
+  const iconColor = syncScroll ? '' : 'text-muted'
   const onClick = useCallback(() => {
     setEditorSyncScroll(!syncScroll)
   }, [syncScroll])
 
   return (
-    <TranslatedIconButton size={'sm'} className={'mx-1'} icon={icon} variant={'light'} onClick={onClick} i18nKey={translation}/>
+    <ToggleButtonGroup type="checkbox" defaultValue={[]} name="dark-mode" className="ml-2" value={[syncScroll]}>
+      <ToggleButton
+        title={ translation }
+        variant={ 'light' }
+        onChange={onClick} value={false}
+        className={'pl-2'}
+      >
+        <ForkAwesomeIcon icon={'refresh'} fixedWidth={true} className={iconColor}/>
+        <Trans i18nKey={translation}/>
+      </ToggleButton>
+    </ToggleButtonGroup>
   )
 }
