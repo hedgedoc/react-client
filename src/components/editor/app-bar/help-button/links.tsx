acdc06b77c01ecec21dec882d0ec2b3c36a763a5
Philip Molares <philip.molares@udo.edu>
2020-08-25 21:41:08 +0200

    fixed help links

--- src/components/editor/app-bar/help-button/links.tsx
+++ src/components/editor/app-bar/help-button/links.tsx
@@ -1,88 +1,83 @@
 import React from 'react'
-import { Card, Col } from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
 import { Trans, useTranslation } from 'react-i18next'
 import { TranslatedExternalLink } from '../../../common/links/translated-external-link'
 
 export const Links: React.FC = () => {
   useTranslation()
   return (
-    <Col lg={4}>
-      <Card>
-        <Card.Header><Trans i18nKey='editor.help.contacts.title'/></Card.Header>
-        <Card.Body>
-          <Card.Text>
-            <ul className="list-unstyled">
-              <li>
-                <TranslatedExternalLink
-                  i18nKey='editor.help.contacts.community'
-                  href='https://community.codimd.org/'
-                  icon='users'
-                  className='text-primary'
-                />
-              </li>
-              <li>
-                <TranslatedExternalLink
-                  i18nKey='editor.help.contacts.meetUsOn'
-                  i18nOption={{ service: 'Matrix' }}
-                  href='https://riot.im/app/#/room/#codimd:matrix.org'
-                  icon='hashtag'
-                  className='text-primary'
-                />
-              </li>
-              <li>
-                <TranslatedExternalLink
-                  i18nKey='editor.help.contacts.reportIssue'
-                  href='https://github.com/codimd/server/issues'
-                  icon='tag'
-                  className='text-primary'
-                />
-              </li>
-              <li>
-                <TranslatedExternalLink
-                  i18nKey='editor.help.contacts.helpTranslating'
-                  href='https://translate.codimd.org/'
-                  icon='language'
-                  className='text-primary'
-                />
-              </li>
-            </ul>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <br/>
-      <Card>
-        <Card.Header><Trans i18nKey='editor.help.documents.title'/></Card.Header>
-        <Card.Body>
-          <Card.Text>
-            <ul className="list-unstyled">
-              <li>
-                <TranslatedExternalLink
-                  i18nKey='editor.help.documents.features'
-                  href='/n/features'
-                  icon='dot-circle-o'
-                  className='text-primary'
-                />
-              </li>
-              <li>
-                <TranslatedExternalLink
-                  i18nKey='editor.help.documents.yamlMetadata'
-                  href='/n/yaml-data'
-                  icon='dot-circle-o'
-                  className='text-primary'
-                />
-              </li>
-              <li>
-                <TranslatedExternalLink
-                  i18nKey='editor.help.documents.slideExample'
-                  href='https://github.com/codimd/server/issues'
-                  icon='dot-circle-o'
-                  className='text-primary'
-                />
-              </li>
-            </ul>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
+    <Row className={'justify-content-center pt-4'}>
+      <Col lg={4}>
+        <h3><Trans i18nKey='editor.help.contacts.title'/></h3>
+        <div>
+          <ul className="list-unstyled">
+            <li>
+              <TranslatedExternalLink
+                i18nKey='editor.help.contacts.community'
+                href='https://community.codimd.org/'
+                icon='users'
+                className='text-primary'
+              />
+            </li>
+            <li>
+              <TranslatedExternalLink
+                i18nKey='editor.help.contacts.meetUsOn'
+                i18nOption={{ service: 'Matrix' }}
+                href='https://riot.im/app/#/room/#codimd:matrix.org'
+                icon='hashtag'
+                className='text-primary'
+              />
+            </li>
+            <li>
+              <TranslatedExternalLink
+                i18nKey='editor.help.contacts.reportIssue'
+                href='https://github.com/codimd/server/issues'
+                icon='tag'
+                className='text-primary'
+              />
+            </li>
+            <li>
+              <TranslatedExternalLink
+                i18nKey='editor.help.contacts.helpTranslating'
+                href='https://translate.codimd.org/'
+                icon='language'
+                className='text-primary'
+              />
+            </li>
+          </ul>
+        </div>
+      </Col>
+      <Col lg={4}>
+        <h3><Trans i18nKey='editor.help.documents.title'/></h3>
+        <div>
+          <ul className="list-unstyled">
+            <li>
+              <TranslatedExternalLink
+                i18nKey='editor.help.documents.features'
+                href='/n/features'
+                icon='dot-circle-o'
+                className='text-primary'
+              />
+            </li>
+            <li>
+              <TranslatedExternalLink
+                i18nKey='editor.help.documents.yamlMetadata'
+                href='/n/yaml-data'
+                icon='dot-circle-o'
+                className='text-primary'
+              />
+            </li>
+            <li>
+              <TranslatedExternalLink
+                i18nKey='editor.help.documents.slideExample'
+                href='https://github.com/codimd/server/issues'
+                icon='dot-circle-o'
+                className='text-primary'
+              />
+            </li>
+          </ul>
+        </div>
+      </Col>
+    </Row>
   )
 }
