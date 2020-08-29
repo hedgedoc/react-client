6d860650f5231ef2b88720de82284e43b97dd150
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-08-29 23:23:31 +0200

    Add error handling
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/markdown-renderer/replace-components/flow/flowchart/flowchart.tsx
+++ src/components/markdown-renderer/replace-components/flow/flowchart/flowchart.tsx
@@ -1,5 +1,7 @@
-import { parse } from "flowchart.js"
-import React, { useEffect, useRef } from 'react'
+import { parse } from 'flowchart.js'
+import React, { useEffect, useRef, useState } from 'react'
+import { Alert } from 'react-bootstrap'
+import { Trans, useTranslation } from 'react-i18next'
 
 export interface FlowChartProps {
   code: string
@@ -7,18 +9,41 @@ export interface FlowChartProps {
 
 export const FlowChart: React.FC<FlowChartProps> = ({ code }) => {
   const diagramRef = useRef<HTMLDivElement>(null)
+  const [error, setError] = useState(false)
+
+  useTranslation()
+
   useEffect(() => {
-    if(diagramRef.current === null) {
+    if (diagramRef.current === null) {
       return
     }
     const parserOutput = parse(code)
-    parserOutput.drawSVG(diagramRef.current, {
-      'line-width': 2,
-      'fill': 'none',
-      'font-size': '16px',
-      'font-family': 'Source Code Pro twemoji, monospace'
-    })
+    try {
+      parserOutput.drawSVG(diagramRef.current, {
+        'line-width': 2,
+        fill: 'none',
+        'font-size': '16px',
+        'font-family': 'Source Code Pro, twemoji, monospace'
+      })
+      setError(false)
+    } catch (error) {
+      setError(true)
+    }
+
+    const currentDiagramRef = diagramRef.current
+
+    return () => {
+      Array.from(currentDiagramRef.children).forEach(value => value.remove())
+    }
   }, [code])
 
-  return <div ref={diagramRef} className={'text-center'}/>
+  if (error) {
+    return (
+      <Alert variant={'danger'}>
+        <Trans i18nKey={'renderer.flowchart.invalidSyntax'}/>
+      </Alert>
+    )
+  } else {
+    return <div ref={diagramRef} className={'text-center'}/>
+  }
 }
