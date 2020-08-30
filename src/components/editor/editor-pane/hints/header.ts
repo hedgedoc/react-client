4b5c6b12ad673c841e6fbebc2fd3dc3fa23f13d2
Philip Molares <philip.molares@udo.edu>
2020-08-30 12:15:41 +0200

    added missing autocompletions: - code-block - container - header - image - link - pdf

---
+++ src/components/editor/editor-pane/hints/header.ts
@@ -0,0 +1,42 @@
+import { Editor, Hint, Hints, Pos } from 'codemirror'
+import { findWordAtCursor, Hinter, search } from './index'
+
+const allowedChars = /(#{1,6})/
+const wordRegExp = /^(\s{0,3})(#{1,6})$/
+const allSupportedHeaders = ['# h1', '## h2', '### h3', '#### h4', '##### h5', '###### h6', '###### tags: `example`']
+
+const headerHint = (editor: Editor): Promise< Hints| null > => {
+  return new Promise((resolve) => {
+    const searchTerm = findWordAtCursor(editor, allowedChars)
+    const searchResult = wordRegExp.exec(searchTerm.text)
+    if (searchResult === null) {
+      resolve(null)
+      return
+    }
+    const term = searchResult[0]
+    if (!term) {
+      resolve(null)
+      return
+    }
+    const suggestions = search(term, allSupportedHeaders)
+    const cursor = editor.getCursor()
+    if (!suggestions) {
+      resolve(null)
+    } else {
+      resolve({
+        list: suggestions.map((suggestion: string): Hint => ({
+          text: suggestion,
+          displayText: suggestion
+        })),
+        from: Pos(cursor.line, searchTerm.start),
+        to: Pos(cursor.line, searchTerm.end)
+      })
+    }
+  })
+}
+
+export const HeaderHinter: Hinter = {
+  allowedChars,
+  wordRegExp,
+  hint: headerHint
+}
