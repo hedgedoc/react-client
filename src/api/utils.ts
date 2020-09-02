28212f997c10b8e392d1c2104f84c27190463174
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-09-02 20:56:29 +0200

    Fix rebase issues
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -8,7 +8,8 @@ export const defaultFetchConfig: Partial<RequestInit> = {
     'Content-Type': 'application/json'
   },
   redirect: 'follow',
-  referrerPolicy: 'no-referrer'
+  referrerPolicy: 'no-referrer',
+  method: 'GET'
 }
 
 export const getApiUrl = (): string => {
@@ -19,4 +20,10 @@ export const expectResponseCode = (response: Response, code = 200): void => {
   if (response.status !== code) {
     throw new Error(`response code is not ${code}`)
   }
+}
+
+export const fetchApiUrl = async (path: string, headers: Partial<RequestInit> = {}, responseCode = 200): Promise<Response> => {
+  const response = await fetch(getApiUrl() + path, { ...headers, ...defaultFetchConfig })
+  expectResponseCode(response, responseCode)
+  return response
 }
