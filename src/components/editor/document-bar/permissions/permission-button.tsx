7c97b107178a424e7e2c67903e55a2c90cbd4909
Philip Molares <philip.molares@udo.edu>
2020-09-03 13:09:16 +0200

    build new permissions modal added owner added user list

---
+++ src/components/editor/document-bar/permissions/permission-button.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment, useState } from 'react'
+import { TranslatedIconButton } from '../../../common/icon-button/translated-icon-button'
+import { PermissionModal } from './permissions-modal'
+
+export const PermissionButton: React.FC = () => {
+  const [showReadOnly, setShowReadOnly] = useState(false)
+
+  return (
+    <Fragment>
+      <TranslatedIconButton size={'sm'} className={'mx-1'} icon={'lock'} variant={'light'} onClick={() => setShowReadOnly(true)} i18nKey={'editor.documentBar.permissions'}/>
+      <PermissionModal show={showReadOnly} onChangeShow={setShowReadOnly}/>
+    </Fragment>
+  )
+}
