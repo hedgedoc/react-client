ef0adf9ef5b616ecd179a85debf39a363e138d2b
Philip Molares <philip.molares@udo.edu>
2020-09-05 19:45:58 +0200

    added 'unkown' to error-boundary title

--- src/components/error-boundary/error-boundary.tsx
+++ src/components/error-boundary/error-boundary.tsx
@@ -32,7 +32,7 @@ export class ErrorBoundary extends Component {
       return (
         <Container className="text-white d-flex flex-column mvh-100">
           <div className='text-white d-flex flex-column align-items-center justify-content-center my-5'>
-            <h1>An error occurred</h1>
+            <h1>An unknown error occurred</h1>
             <p>Don't worry, this happens sometimes. If this is the first time you see this page then try reloading the app.</p>
             If you can reproduce this error, then we would be glad if you <a href={frontendVersion.issueTrackerUrl} target='_blank' rel='noopener noreferrer'>open an issue on github</a> or <a href={'https://riot.im/app/#/room/#hedgedoc:matrix.org'} target='_blank' rel='noopener noreferrer' dir='auto'>contact us on matrix.</a>
             <Button onClick={() => this.refreshPage()} title={'Reload Page'} className={'mt-4'}>
