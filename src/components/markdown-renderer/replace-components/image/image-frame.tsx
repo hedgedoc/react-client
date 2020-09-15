f1f5148ca24ef03cee35cc97125ec1792ac589a6
Philip Molares <philip.molares@udo.edu>
2020-09-15 09:52:15 +0200

    added expand image button

--- src/components/markdown-renderer/replace-components/image/image-frame.tsx
+++ src/components/markdown-renderer/replace-components/image/image-frame.tsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
+import { Button, Modal } from 'react-bootstrap'
+import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
 import { getProxiedUrl } from '../../../../api/media'
 import { ApplicationState } from '../../../../redux'
+import { ForkAwesomeIcon } from '../../../common/fork-awesome/fork-awesome-icon'
+import { CommonModal } from '../../../common/modals/common-modal'
 
-export const ImageFrame: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = ({ alt, src, title, ...props }) => {
+export const ImageFrame: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = ({ src, title, ...props }) => {
   const [imageUrl, setImageUrl] = useState('')
   const imageProxyEnabled = useSelector((state: ApplicationState) => state.config.useImageProxy)
 
@@ -18,11 +22,39 @@ export const ImageFrame: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = (
 
   if (imageProxyEnabled) {
     return (
-      <img alt={alt} src={imageUrl} title={title ?? ''} {...props}/>
+      <Frame src={imageUrl} title={title ?? ''} {...props}/>
     )
   }
 
   return (
-    <img alt={alt} src={src ?? ''} title={title ?? ''} {...props}/>
+    <Frame src={src ?? ''} title={title ?? ''} {...props}/>
+  )
+}
+
+const Frame: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = ({ alt, ...props }) => {
+  const { t } = useTranslation()
+  const [showFullscreenImage, setShowFullscreenImage] = useState(false)
+
+  return (
+    <Fragment>
+      <img alt={alt} {...props}/>
+      <div className={'text-right'} style={{ marginTop: '-31px' }}>
+        <Button size={'sm'} variant={'dark'} onClick={() => setShowFullscreenImage(true)} title={t('renderer.image.expandImage')}>
+          <ForkAwesomeIcon icon={'expand'}/>
+        </Button>
+      </div>
+      <CommonModal
+        show={showFullscreenImage}
+        onHide={() => setShowFullscreenImage(false)}
+        title={alt ?? ''}
+        closeButton={true}
+        size={'xl'}
+        icon={'picture-o'}
+      >
+        <Modal.Body>
+          <img alt={alt} {...props} className={'w-100'}/>
+        </Modal.Body>
+      </CommonModal>
+    </Fragment>
   )
 }
