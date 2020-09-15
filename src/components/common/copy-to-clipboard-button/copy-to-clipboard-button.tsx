ffee71f8145a965691ba04d8bbd83e4cfed9f416
Philip Molares <philip.molares@udo.edu>
2020-09-15 10:03:37 +0200

    better error handling on copy error

--- src/components/common/copy-to-clipboard-button/copy-to-clipboard-button.tsx
+++ src/components/common/copy-to-clipboard-button/copy-to-clipboard-button.tsx
@@ -3,6 +3,7 @@ import { Button, Overlay, Tooltip } from 'react-bootstrap'
 import { Variant } from 'react-bootstrap/types'
 import { Trans, useTranslation } from 'react-i18next'
 import { ForkAwesomeIcon } from '../fork-awesome/fork-awesome-icon'
+import { ShowIf } from '../show-if/show-if'
 
 export interface CopyToClipboardButtonProps {
   content: string
@@ -14,13 +15,17 @@ export const CopyToClipboardButton: React.FC<CopyToClipboardButtonProps> = ({ co
   const { t } = useTranslation()
   const button = useRef<HTMLButtonElement>(null)
   const [showCopiedTooltip, setShowCopiedTooltip] = useState(false)
+  const [error, setError] = useState(false)
 
   const copyToClipboard = useCallback((content: string) => {
     navigator.clipboard.writeText(content).then(() => {
+      setError(false)
+    }).catch(() => {
+      setError(true)
+      console.error("couldn't copy")
+    }).finally(() => {
       setShowCopiedTooltip(true)
       setTimeout(() => { setShowCopiedTooltip(false) }, 2000)
-    }).catch(() => {
-      console.error("couldn't copy")
     })
   }, [])
 
@@ -29,7 +34,12 @@ export const CopyToClipboardButton: React.FC<CopyToClipboardButtonProps> = ({ co
       <Overlay target={button} show={showCopiedTooltip} placement="top">
         {(props) => (
           <Tooltip id={'copied_' + content} {...props}>
-            <Trans i18nKey={'common.successfullyCopied'}/>
+            <ShowIf condition={error}>
+              <Trans i18nKey={'common.copyError'}/>
+            </ShowIf>
+            <ShowIf condition={!error}>
+              <Trans i18nKey={'common.successfullyCopied'}/>
+            </ShowIf>
           </Tooltip>
         )}
       </Overlay>
