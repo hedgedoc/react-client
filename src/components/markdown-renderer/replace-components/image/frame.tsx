63ef24d02d1003e9b393264ca6403e5f09612c01
Philip Molares <philip.molares@udo.edu>
2020-09-19 12:00:09 +0200

    fixed modal header

--- src/components/markdown-renderer/replace-components/image/frame.tsx
+++ src/components/markdown-renderer/replace-components/image/frame.tsx
@@ -16,9 +16,12 @@ export const Frame: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = ({ alt
         closeButton={true}
         size={'xl'}
       >
-        <Modal.Header closeButton={true} className={'flex-row'}>
-          <ForkAwesomeIcon icon={'picture-o'}/>
-          <span>{alt ?? ''}</span>
+        <Modal.Header closeButton={true}>
+          <Modal.Title>
+            <ForkAwesomeIcon icon={'picture-o'}/>
+            &nbsp;
+            <span>{alt ?? ''}</span>
+          </Modal.Title>
         </Modal.Header>
         <img alt={alt} {...props} className={'w-100'}/>
       </Modal>
