b5d33168332b61f40e1673623b22457793ad1c33
Philip Molares <git@molar.es>
2020-10-07 23:20:15 +0200

    Update src/components/markdown-renderer/hooks/use-extract-first-headline.ts
    Co-authored-by: Erik Michelson <github@erik.michelson.eu>

--- src/components/markdown-renderer/hooks/use-extract-first-headline.ts
+++ src/components/markdown-renderer/hooks/use-extract-first-headline.ts
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect } from 'react'
 
-export const useExtractFirstHeadline = (documentElement: React.RefObject<HTMLDivElement>, content: string, onFirstHeadingChange?: (firstHeading: string | undefined) => void) => {
+export const useExtractFirstHeadline = (documentElement: React.RefObject<HTMLDivElement>, content: string, onFirstHeadingChange?: (firstHeading: string | undefined) => void): string => {
   const extractInnerText = useCallback((node: ChildNode): string => {
     let innerText = ''
     if (node.childNodes && node.childNodes.length > 0) {
