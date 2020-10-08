76ff842e2c242e5cc4e368ad9dfe3963df0c6558
Philip Molares <philip.molares@udo.edu>
2020-10-08 22:09:39 +0200

    started work on disabling pan and zoom on markmap

--- src/components/markdown-renderer/replace-components/markmap/markmap-frame.tsx
+++ src/components/markdown-renderer/replace-components/markmap/markmap-frame.tsx
@@ -1,11 +1,44 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 export interface MarkmapFrameProps {
   code: string
 }
 
+const wheelHandler = (event: Event): void => {
+  console.log('event', event)
+  event.stopPropagation()
+}
+
+const mousedownHandler = (event: MouseEvent): void => {
+  console.log('event', event)
+  if (event.button === 0) {
+    event.stopPropagation()
+  }
+}
+
+const touchHandler = (event: TouchEvent): void => {
+  console.log('event', event)
+  event.stopPropagation()
+}
+
 export const MarkmapFrame: React.FC<MarkmapFrameProps> = ({ code }) => {
   const diagramContainer = useRef<HTMLDivElement>(null)
+  const [disablePanAndZoom, setDisablePanAndZoom] = useState<boolean>()
+
+  useEffect(() => {
+    if (diagramContainer.current) {
+      if (disablePanAndZoom) {
+        console.log('addedEventListener')
+        diagramContainer.current.addEventListener('wheel', wheelHandler)
+        diagramContainer.current.addEventListener('mousedown', mousedownHandler)
+        diagramContainer.current.addEventListener('touchstart', touchHandler)
+      } else {
+        diagramContainer.current.removeEventListener('wheel', wheelHandler)
+        diagramContainer.current.removeEventListener('mousedown', mousedownHandler)
+        diagramContainer.current.removeEventListener('touchstart', touchHandler)
+      }
+    }
+  }, [diagramContainer, disablePanAndZoom])
 
   useEffect(() => {
     if (!diagramContainer.current) {
@@ -30,6 +63,7 @@ export const MarkmapFrame: React.FC<MarkmapFrameProps> = ({ code }) => {
             .catch(err => console.error(err))
         }
         view.Markmap.create(svg, {}, root)
+        setDisablePanAndZoom(true)
       }).catch(() => { console.error('error while loading markmap') })
   }, [code])
 
