3ed507151f3ff49d5b96bbc6b31047f4cc3d86ad
Philip Molares <philip.molares@udo.edu>
2020-11-22 13:19:02 +0100

    package.json and craco.config.js should not be under CC0
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,7 @@
 /*
  * SPDX-FileCopyrightText: 2020 The HedgeDoc developers (see AUTHORS file)
  *
- * SPDX-License-Identifier: CC0-1.0
+ * SPDX-License-Identifier: AGPL-3.0-only
  */
 
 const CopyPlugin = require('copy-webpack-plugin');
