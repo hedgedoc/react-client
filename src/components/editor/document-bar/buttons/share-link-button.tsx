e1c7c662ba5355d682849ffdb5375d0f60f55c6f
Erik Michelson <github@erik.michelson.eu>
2020-11-29 01:44:12 +0100

    Add additional fields and labels in share link dialog
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

--- src/components/editor/document-bar/buttons/share-link-button.tsx
+++ src/components/editor/document-bar/buttons/share-link-button.tsx
@@ -4,27 +4,43 @@ SPDX-FileCopyrightText: 2020 The HedgeDoc developers (see AUTHORS file)
 SPDX-License-Identifier: AGPL-3.0-only
 */
 
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useCallback, useState } from 'react'
 import { Modal } from 'react-bootstrap'
-import { Trans } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import { CopyableField } from '../../../common/copyable/copyable-field/copyable-field'
 import { TranslatedIconButton } from '../../../common/icon-button/translated-icon-button'
 import { CommonModal } from '../../../common/modals/common-modal'
+import { ShowIf } from '../../../common/show-if/show-if'
 
 export const ShareLinkButton: React.FC = () => {
-  const [showReadOnly, setShowReadOnly] = useState(false)
+  const { t } = useTranslation()
+  const [showShareDialog, setShowShareDialog] = useState(false)
 
   return (
     <Fragment>
-      <TranslatedIconButton size={'sm'} className={'mx-1'} icon={'share'} variant={'light'} onClick={() => setShowReadOnly(true)} i18nKey={'editor.documentBar.shareLink'}/>
+      <TranslatedIconButton
+        size={'sm'}
+        className={'mx-1'}
+        icon={'share'}
+        variant={'light'}
+        onClick={() => setShowShareDialog(true)} i18nKey={'editor.documentBar.shareLink'}
+      />
       <CommonModal
-        show={showReadOnly}
-        onHide={() => setShowReadOnly(false)}
+        show={showShareDialog}
+        onHide={() => setShowShareDialog(false)}
         closeButton={true}
         titleI18nKey={'editor.modal.shareLink.title'}>
         <Modal.Body>
-          <span className={'my-4'}><Trans i18nKey={'editor.modal.shareLink.viewOnlyDescription'}/></span>
-          <CopyableField content={'https://example.com'}/>
+          <Trans i18nKey={'editor.modal.shareLink.editorDescription'}/>
+          <CopyableField content={'edit link'}/>
+          <ShowIf condition={true}>
+            <Trans i18nKey={'editor.modal.shareLink.slidesDescription'}/>
+            <CopyableField content={'slides link'}/>
+          </ShowIf>
+          <ShowIf condition={true}>
+            <Trans i18nKey={'editor.modal.shareLink.viewOnlyDescription'}/>
+            <CopyableField content={'view link'}/>
+          </ShowIf>
         </Modal.Body>
       </CommonModal>
     </Fragment>
