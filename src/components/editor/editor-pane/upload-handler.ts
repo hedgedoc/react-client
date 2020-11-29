ee0fd67eb25a387c9c8baf2c71f8d66482f39eaa
Philip Molares <philip.molares@udo.edu>
2020-11-29 11:58:22 +0100

    added changes suggested by ErikMichelson and mrdrogdrog
    Co-authored by: Erik Michelson <github@erik.michelson.eu>
    Co-authored by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/components/editor/editor-pane/upload-handler.ts
+++ src/components/editor/editor-pane/upload-handler.ts
@@ -10,25 +10,35 @@ import { uploadFile } from '../../../api/media'
 import { store } from '../../../redux'
 
 export const handleUpload = (files: FileList | null, editor: Editor | undefined): void => {
-  if (files && editor) {
-    const file = files[0]
-    const mimeType = file.type
-    const cursor = editor.getCursor()
-    const uploadPlaceholder = `![${i18n.t('editor.upload.uploadFile', { fileName: file.name })}]()`
-    const noteId = store.getState().documentContent.noteId
-    editor.replaceRange(uploadPlaceholder, cursor, cursor, '+input')
-    uploadFile(noteId, mimeType, file)
-      .then(({ link }) => {
-        editor.replaceRange(`![](${link})`, cursor, {
-          line: cursor.line,
-          ch: cursor.ch + uploadPlaceholder.length
-        }, '+input')
-      })
-      .catch(() => {
-        editor.replaceRange('', cursor, {
-          line: cursor.line,
-          ch: cursor.ch + uploadPlaceholder.length
-        }, '+input')
-      })
+  if (!files || !editor) {
+    return
+  }
+  const file = files[0]
+  const mimeType = file.type
+  const cursor = editor.getCursor()
+  const uploadPlaceholder = `![${i18n.t('editor.upload.uploadFile', { fileName: file.name })}]()`
+  const noteId = store.getState().documentContent.noteId
+  editor.replaceRange(uploadPlaceholder, cursor, cursor, '+input')
+  uploadFile(noteId, mimeType, file)
+    .then(({ link }) => {
+      editor.replaceRange(getCorrectSyntaxForLink(mimeType, link), cursor, {
+        line: cursor.line,
+        ch: cursor.ch + uploadPlaceholder.length
+      }, '+input')
+    })
+    .catch(() => {
+      editor.replaceRange('', cursor, {
+        line: cursor.line,
+        ch: cursor.ch + uploadPlaceholder.length
+      }, '+input')
+    })
+}
+
+const getCorrectSyntaxForLink = (mimeType: string, link: string): string => {
+  switch (mimeType) {
+    case 'application/pdf':
+      return `{%pdf ${link} %}`
+    default:
+      return `![](${link})`
   }
 }
