b76baa116df6c0b5ea5ece85f472787becab522a
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-12-14 23:35:50 +0100

    Use correct translations
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/editor/editor-pane/tool-bar/editor-preferences/editor-preference-ligatures-select.tsx
+++ src/components/editor/editor-pane/tool-bar/editor-preferences/editor-preference-ligatures-select.tsx
@@ -21,8 +21,8 @@ export const EditorPreferenceLigaturesSelect: React.FC = () => {
   return (
     <EditorPreferenceInput onChange={saveLigatures} value={ligaturesEnabled} property={"ligatures"}
                            type={EditorPreferenceInputType.BOOLEAN}>
-      <option value='false'>{t(`common.off`)}</option>
-      <option value='true'>{t(`common.on`)}</option>
+      <option value='false'>{t(`common.yes`)}</option>
+      <option value='true'>{t(`common.no`)}</option>
     </EditorPreferenceInput>
   )
 }
