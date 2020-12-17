476f1c7cb2b98191bbefdc5737f579a47ecd0825
Erik Michelson <github@erik.michelson.eu>
2020-12-17 22:17:54 +0100

    Fix bug that pathname is always empty
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

--- src/hooks/common/use-frontend-base-url.ts
+++ src/hooks/common/use-frontend-base-url.ts
@@ -9,5 +9,5 @@ import { useLocation } from 'react-router'
 export const useFrontendBaseUrl = (): string => {
   const { pathname } = useLocation()
 
-  return window.location.pathname.replace(pathname, '')
+  return window.location.href.replace(pathname, '')
 }
