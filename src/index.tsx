7f80a7f15d9b2f6d1d867e27e4b2dd9cda48b4c5
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-12-27 16:54:52 +0100

    Add test mode
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/index.tsx
+++ src/index.tsx
@@ -80,6 +80,10 @@ ReactDOM.render(
   , document.getElementById('root')
 )
 
+if (process.env.REACT_APP_TEST_MODE) {
+  console.log("This build runs in test mode")
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
