552390af9f27531c9498e8f7f130eadd7ea4fa1b
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2021-01-06 17:39:12 +0100

    Add license stuff
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/editor/document-renderer-pane/use-adapted-line-markers-callback.ts
+++ src/components/editor/document-renderer-pane/use-adapted-line-markers-callback.ts
@@ -1,3 +1,9 @@
+/*
+SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+
+SPDX-License-Identifier: AGPL-3.0-only
+*/
+
 import { RefObject, useCallback } from 'react'
 import { LineMarkerPosition } from '../../markdown-renderer/types'
 
