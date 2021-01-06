4040549be679df896a03c11730448c59cf67ad6d
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2021-01-06 17:32:12 +0100

    Move yaml-array warning to documentrenderpane
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

---
+++ src/components/editor/document-renderer-pane/use-adapted-line-markers-callback.ts
@@ -0,0 +1,19 @@
+import { RefObject, useCallback } from 'react'
+import { LineMarkerPosition } from '../../markdown-renderer/types'
+
+export const useAdaptedLineMarkerCallback = (documentRenderPaneRef: RefObject<HTMLDivElement> | undefined,
+                                             rendererRef: RefObject<HTMLDivElement | null>,
+                                             onLineMarkerPositionChanged: ((lineMarkerPosition: LineMarkerPosition[]) => void) | undefined): ((lineMarkerPosition: LineMarkerPosition[]) => void) => {
+  return useCallback((linkMarkerPositions) => {
+    if (!onLineMarkerPositionChanged) {
+      return
+    }
+    const documentRenderPaneTop = (documentRenderPaneRef?.current?.offsetTop ?? 0)
+    const rendererTop = (rendererRef.current?.offsetTop ?? 0)
+    const offset = rendererTop - documentRenderPaneTop
+    onLineMarkerPositionChanged(linkMarkerPositions.map(oldMarker => ({
+      line: oldMarker.line,
+      position: oldMarker.position + offset
+    })))
+  }, [documentRenderPaneRef, onLineMarkerPositionChanged, rendererRef])
+}
