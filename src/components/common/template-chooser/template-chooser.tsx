534112f734edafebdab310bb1df29b949f0fce31
Erik Michelson <github@erik.michelson.eu>
2021-01-08 02:37:01 +0100

    Added modal for template chooser
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

---
+++ src/components/common/template-chooser/template-chooser.tsx
@@ -0,0 +1,60 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React, { ChangeEvent, useCallback, useEffect, useState } from 'react'
+import { Button, Col, Form, Modal } from 'react-bootstrap'
+import { Trans } from 'react-i18next'
+import { useSelector } from 'react-redux'
+import { ApplicationState } from '../../../redux'
+import { CommonModal, CommonModalProps } from '../modals/common-modal'
+import { ShowIf } from '../show-if/show-if'
+
+export const TemplateChooser: React.FC<Pick<CommonModalProps, "show" | "onHide">> = ({ show, onHide }) => {
+  const isFreeModeEnabled = useSelector((state: ApplicationState) => state.config.freeUrlMode)
+  const [customAlias, setCustomAlias] = useState('')
+  const [noteCreatable, setNoteCreatable] = useState(false)
+  const [selectedTemplate, setSelectedTemplate] = useState('')
+
+  const onChangeCustomAliasField = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setCustomAlias(event.target.value)
+  }, [])
+
+  useEffect(() => {
+    const invalid = selectedTemplate === '' || /^\W*$/.test(customAlias)
+    setNoteCreatable(!invalid)
+  }, [customAlias, selectedTemplate])
+
+  return (
+    <CommonModal
+      show={show}
+      onHide={onHide}
+      closeButton={true}
+      size={'lg'}
+      titleI18nKey={'common.modal.chooseTemplate.title'}
+    >
+      <Modal.Body>
+        <span>Templates will be listed here</span>
+        <ShowIf condition={isFreeModeEnabled}>
+          <hr />
+          <Form>
+            <Form.Row>
+              <Form.Label column={'sm'} lg={2}>
+                <Trans i18nKey={'common.modal.chooseTemplate.customNoteAlias'} />
+              </Form.Label>
+              <Col>
+                <Form.Control type={'text'} size={'sm'} value={customAlias} onChange={onChangeCustomAliasField} />
+              </Col>
+            </Form.Row>
+          </Form>
+        </ShowIf>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant={'primary'} disabled={!noteCreatable}>
+          <Trans i18nKey={'common.modal.chooseTemplate.createFromTemplate'} />
+        </Button>
+      </Modal.Footer>
+    </CommonModal>
+  )
+}
--- src/components/landing-layout/navigation/new-note-button.tsx
+++ src/components/landing-layout/navigation/new-note-button.tsx
@@ -3,11 +3,12 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React from 'react'
+import React, { useState, Fragment, useCallback } from 'react'
 import { Button, ButtonGroup, Dropdown } from 'react-bootstrap'
 import { Trans, useTranslation } from 'react-i18next'
 import { LinkContainer } from 'react-router-bootstrap'
 import { ForkAwesomeIcon } from '../../common/fork-awesome/fork-awesome-icon'
+import { TemplateChooser } from '../../common/template-chooser/template-chooser'
 
 export interface NewNoteButtonProps {
   i18nKey: string
@@ -16,32 +17,45 @@ export interface NewNoteButtonProps {
 
 export const NewNoteButton: React.FC<NewNoteButtonProps> = ({ i18nKey, className }) => {
   const { t } = useTranslation()
+
+  const [showTemplateChooser, setShowTemplateChooser] = useState(false)
+
+  const onShowTemplateChooser = useCallback(() => {
+    setShowTemplateChooser(true)
+  }, [setShowTemplateChooser])
+  const onHideTemplateChooser = useCallback(() => {
+    setShowTemplateChooser(false)
+  }, [setShowTemplateChooser])
+
   return (
-    <Dropdown
-      as={ButtonGroup}
-      className={className ?? ''}
-      size={'sm'}
-      variant={'primary'}
-    >
-      <LinkContainer to={'/new'} title={t(i18nKey)}>
-        <Button
-          className={`d-inline-flex align-items-center`}>
-          <ForkAwesomeIcon icon={'plus'} className={'mx-1'} />
-          <span>
+    <Fragment>
+      <Dropdown
+        as={ButtonGroup}
+        className={className ?? ''}
+        size={'sm'}
+        variant={'primary'}
+      >
+        <LinkContainer to={'/new'} title={t(i18nKey)}>
+          <Button
+            className={`d-inline-flex align-items-center`}>
+            <ForkAwesomeIcon icon={'plus'} className={'mx-1'} />
+            <span>
           <Trans i18nKey={i18nKey} />
         </span>
-        </Button>
-      </LinkContainer>
+          </Button>
+        </LinkContainer>
 
-      <Dropdown.Toggle split={true} />
+        <Dropdown.Toggle split={true} />
 
-      <Dropdown.Menu>
-        <Dropdown.Item>
-          <Trans i18nKey={'landing.navigation.newFromTemplate'} />
-        </Dropdown.Item>
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={onShowTemplateChooser}>
+            <Trans i18nKey={'landing.navigation.newFromTemplate'} />
+          </Dropdown.Item>
 
-        <Dropdown.Divider />
-      </Dropdown.Menu>
-    </Dropdown>
+          <Dropdown.Divider />
+        </Dropdown.Menu>
+      </Dropdown>
+      <TemplateChooser show={showTemplateChooser} onHide={onHideTemplateChooser} />
+    </Fragment>
   )
 }
