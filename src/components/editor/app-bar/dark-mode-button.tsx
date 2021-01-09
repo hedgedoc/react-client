0018981ebc7306e988a4ef8e011b574a9716118f
Erik Michelson <github@erik.michelson.eu>
2021-01-09 01:58:48 +0100

    Fix as ToggleButtons require now enums instead of booleans
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

--- src/components/editor/app-bar/dark-mode-button.tsx
+++ src/components/editor/app-bar/dark-mode-button.tsx
@@ -4,30 +4,47 @@ SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
 SPDX-License-Identifier: AGPL-3.0-only
 */
 
-import React from 'react'
+import React, { useMemo } from 'react'
 import { ToggleButton, ToggleButtonGroup } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import { useIsDarkModeActivated } from '../../../hooks/common/use-is-dark-mode-activated'
 import { setDarkMode } from '../../../redux/dark-mode/methods'
 import { ForkAwesomeIcon } from '../../common/fork-awesome/fork-awesome-icon'
 
+enum DarkModeState {
+  DARK,
+  LIGHT
+}
+
 const DarkModeButton: React.FC = () => {
   const { t } = useTranslation()
   const darkModeEnabled = useIsDarkModeActivated()
+
+  const darkModeState = useMemo(() => {
+    return darkModeEnabled ? DarkModeState.DARK : DarkModeState.LIGHT
+  }, [darkModeEnabled])
 
   return (
     <ToggleButtonGroup
       type="radio"
       name="dark-mode"
-      value={darkModeEnabled}
+      value={darkModeState}
       className="ml-2"
-      onChange={(value: boolean) => {
-        setDarkMode(value)
-      }}>
-      <ToggleButton value={true} variant="outline-secondary" title={t('editor.darkMode.switchToDark')}>
+    >
+      <ToggleButton
+        value={DarkModeState.DARK}
+        variant="outline-secondary"
+        title={t('editor.darkMode.switchToDark')}
+        onChange={() => setDarkMode(true)}
+      >
         <ForkAwesomeIcon icon="moon"/>
       </ToggleButton>
-      <ToggleButton value={false} variant="outline-secondary" title={t('editor.darkMode.switchToLight')}>
+      <ToggleButton
+        value={DarkModeState.LIGHT}
+        variant="outline-secondary"
+        title={t('editor.darkMode.switchToLight')}
+        onChange={() => setDarkMode(false)}
+      >
         <ForkAwesomeIcon icon="sun-o"/>
       </ToggleButton>
     </ToggleButtonGroup>
--- src/components/editor/app-bar/sync-scroll-buttons/sync-scroll-buttons.tsx
+++ src/components/editor/app-bar/sync-scroll-buttons/sync-scroll-buttons.tsx
@@ -4,7 +4,7 @@ SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
 SPDX-License-Identifier: AGPL-3.0-only
 */
 
-import React from 'react'
+import React, { useMemo } from 'react'
 import { ToggleButton, ToggleButtonGroup } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
@@ -14,24 +14,35 @@ import { ReactComponent as DisabledScrollIcon } from './disabledScroll.svg'
 import { ReactComponent as EnabledScrollIcon } from './enabledScroll.svg'
 import './sync-scroll-buttons.scss'
 
+enum SyncScrollState {
+  SYNCED,
+  UNSYNCED
+}
+
 export const SyncScrollButtons: React.FC = () => {
   const syncScroll: boolean = useSelector((state: ApplicationState) => state.editorConfig.syncScroll)
   const { t } = useTranslation()
 
+  const syncScrollState = useMemo(() => {
+    return syncScroll ? SyncScrollState.SYNCED : SyncScrollState.UNSYNCED
+  }, [syncScroll])
+
   return (
     <ToggleButtonGroup type="radio" defaultValue={[]} name="sync-scroll" className={'ml-2 sync-scroll-buttons'}
-      value={[syncScroll]}>
+      value={syncScrollState}>
       <ToggleButton
         variant={'outline-secondary'}
         title={t('editor.appBar.syncScroll.enable')}
-        onChange={() => setEditorSyncScroll(true)} value={true}
+        onChange={() => setEditorSyncScroll(true)}
+        value={SyncScrollState.SYNCED}
       >
         <EnabledScrollIcon/>
       </ToggleButton>
       <ToggleButton
         variant={'outline-secondary'}
         title={t('editor.appBar.syncScroll.disable')}
-        onChange={() => setEditorSyncScroll(false)} value={false}
+        onChange={() => setEditorSyncScroll(false)}
+        value={SyncScrollState.UNSYNCED}
       >
         <DisabledScrollIcon/>
       </ToggleButton>
