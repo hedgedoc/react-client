e8db7006f1eb8914781cdc8a15429b0316ec3613
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2021-01-10 20:44:42 +0100

    Remove unnecessary before code
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Fix codemirror selection in maxLength
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Restructure tests
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Fix test
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Fix test
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Improve tests
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Fix toolbar tests
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Indent chained calls
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Fix indentation in tests
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Refactor code
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Rename alias
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Remove alias
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Extend test message
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Fix race condition
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Fix race condition.. AGAIN
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Fix counter continue in highlighted-fence-replacer.tsx
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Remove unnecessary before code
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Make line number optional
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Fix codemirror selection in maxLength
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Extend test message
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Add tests
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Merge code.spec.ts and highlightedCodeBlock.spec.ts
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Revert changes to yarn.lock
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Restructure tests
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Remove file
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Format file
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Format file
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Format file
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    replace alias
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Add test for line number continuation
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
    
    Remove line
    
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

---
+++ cypress/integration/highlightedCodeBlock.spec.ts
@@ -0,0 +1,134 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+describe('Code', () => {
+  beforeEach(() => {
+    cy.visit('/n/test', {
+      onBeforeLoad (win: Window): void {
+        cy.spy(win.navigator.clipboard, 'writeText').as('copy')
+      }
+    })
+    cy.get('.btn.active.btn-outline-secondary > i.fa-columns')
+      .should('exist')
+
+    cy.get('.CodeMirror ')
+      .click()
+    cy.get('.CodeMirror textarea')
+      .as('codeinput')
+  })
+
+  describe('with just the language', () => {
+    it('doesn\'t show a gutter', () => {
+      cy.get('@codeinput')
+        .fill('```javascript \nlet x = 0\n```')
+      cy.get('.markdown-body > pre > code.hljs')
+        .should('be.visible')
+        .should('not.have.class', 'showGutter')
+      cy.get('.markdown-body > pre > code.hljs > .linenumber')
+        .should('not.exist')
+    })
+
+    describe('and line wrapping', () => {
+      it('doesn\'t show a gutter', () => {
+        cy.get('@codeinput')
+          .fill('```javascript! \nlet x = 0\n```')
+        cy.get('.markdown-body > pre > code.hljs')
+          .should('be.visible')
+          .should('not.have.class', 'showGutter')
+          .should('have.class', 'wrapLines')
+        cy.get('.markdown-body > pre > code.hljs > .linenumber')
+          .should('not.exist')
+      })
+    })
+  })
+
+  describe('with the language and show gutter', () => {
+    it('shows the correct line number', () => {
+      cy.get('@codeinput')
+        .fill('```javascript= \nlet x = 0\n```')
+      cy.get('.markdown-body > pre > code.hljs')
+        .should('be.visible')
+        .should('have.class', 'showGutter')
+      cy.get('.markdown-body > pre > code.hljs > .linenumber')
+        .should('exist')
+        .styleValueOfBefore('content')
+        .should('eq', '1')
+    })
+
+    describe('and line wrapping', () => {
+      it('shows the correct line number', () => {
+        cy.get('@codeinput')
+          .fill('```javascript=! \nlet x = 0\n```')
+        cy.get('.markdown-body > pre > code.hljs')
+          .should('be.visible')
+          .should('have.class', 'showGutter')
+          .should('have.class', 'wrapLines')
+        cy.get('.markdown-body > pre > code.hljs > .linenumber')
+          .should('exist')
+          .styleValueOfBefore('content')
+          .should('eq', '1')
+      })
+    })
+  })
+
+  describe('with the language, show gutter with a start number', () => {
+    it('shows the correct line number', () => {
+      cy.get('@codeinput')
+        .fill('```javascript=100 \nlet x = 0\n```')
+      cy.get('.markdown-body > pre > code.hljs')
+        .should('be.visible')
+        .should('have.class', 'showGutter')
+      cy.get('.markdown-body > pre > code.hljs > .linenumber')
+        .should('exist')
+        .styleValueOfBefore('content')
+        .should('eq', '100')
+    })
+
+    it('shows the correct line number and continues in another codeblock', () => {
+      cy.get('@codeinput')
+        .fill('```javascript=100 \nlet x = 0\nlet y = 1\n```\n\n```javascript=+\nlet y = 2\n```\n')
+      cy.get('.markdown-body > pre > code.hljs')
+        .should('be.visible')
+        .should('have.class', 'showGutter')
+      cy.get('.markdown-body > pre > code.hljs')
+        .first()
+        .find('.linenumber')
+        .should('exist')
+        .styleValueOfBefore('content')
+        .should('eq', '100')
+      cy.get('.markdown-body > pre > code.hljs')
+        .last()
+        .find('.linenumber')
+        .should('exist')
+        .styleValueOfBefore('content')
+        .should('eq', '102')
+    })
+
+    describe('and line wrapping', () => {
+      it('shows the correct line number', () => {
+        cy.get('@codeinput')
+          .fill('```javascript=100! \nlet x = 0\n```')
+        cy.get('.markdown-body > pre > code.hljs')
+          .should('be.visible')
+          .should('have.class', 'showGutter')
+          .should('have.class', 'wrapLines')
+        cy.get('.markdown-body > pre > code.hljs > .linenumber')
+          .should('exist')
+          .styleValueOfBefore('content')
+          .should('eq', '100')
+      })
+    })
+  })
+
+  it('has a working copy button', () => {
+    cy.get('@codeinput')
+      .fill('```javascript \nlet x = 0\n```')
+    cy.get('.markdown-body > pre > div > button > i')
+      .should('have.class', 'fa-files-o')
+      .click()
+    cy.get('@copy').should('be.calledWithExactly', 'let x = 0\n')
+  })
+})
