16275f18af8edb1dbd42c72e1c82bacd4b86f59c
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2021-01-24 21:16:17 +0100

    Reformat code
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -145,14 +145,14 @@ export const Editor: React.FC = () => {
             showRight={editorMode === EditorMode.PREVIEW || editorMode === EditorMode.BOTH}
             right={
               <DocumentIframe markdownContent={markdownContent}
-                            onMakeScrollSource={setRendererToScrollSource}
-                onFirstHeadingChange={onFirstHeadingChange}
-                onTaskCheckedChange={onTaskCheckedChange
-                }
-                onMetadataChange={onMetadataChange}
-                onScroll={onMarkdownRendererScroll}
+                              onMakeScrollSource={setRendererToScrollSource}
+                              onFirstHeadingChange={onFirstHeadingChange}
+                              onTaskCheckedChange={onTaskCheckedChange
+                              }
+                              onMetadataChange={onMetadataChange}
+                              onScroll={onMarkdownRendererScroll}
 
-                wide={editorMode === EditorMode.PREVIEW}scrollState={scrollState.rendererScrollState}
+                              wide={editorMode === EditorMode.PREVIEW} scrollState={scrollState.rendererScrollState}
               />
             }
             containerClassName={'overflow-hidden'}/>
