90d161478c9840e900ee68188b08a66ce93d7ad0
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2021-02-05 19:43:48 +0100

    Insert copyright
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/intro-page/requests.ts
+++ src/components/intro-page/requests.ts
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { defaultFetchConfig, expectResponseCode } from '../../api/utils'
 
 export const getFrontPageContent = async (pathname: string): Promise<string> => {
