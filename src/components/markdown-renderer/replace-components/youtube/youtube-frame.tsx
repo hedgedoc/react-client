c4932f27a77b0aa5311987e3e7c530d019a0b645
Erik Michelson <github@erik.michelson.eu>
2021-02-08 21:57:34 +0100

    Make YouTube frame use explicit protocol
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

--- src/components/markdown-renderer/replace-components/youtube/youtube-frame.tsx
+++ src/components/markdown-renderer/replace-components/youtube/youtube-frame.tsx
@@ -17,7 +17,7 @@ export const YouTubeFrame: React.FC<YouTubeFrameProps> = ({ id }) => {
                        previewContainerClassName={ 'embed-responsive-item' } hoverIcon={ 'youtube-play' }
                        loadingImageUrl={ `//i.ytimg.com/vi/${ id }/maxresdefault.jpg` }>
       <iframe className='embed-responsive-item' title={ `youtube video of ${ id }` }
-              src={ `//www.youtube-nocookie.com/embed/${ id }?autoplay=1` }
+              src={ `https://www.youtube-nocookie.com/embed/${ id }?autoplay=1` }
               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"/>
     </OneClickEmbedding>
   )
