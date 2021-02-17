e6afc679503afc3f3cb4d486979f9029e1e47978
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2021-02-17 20:15:00 +0100

    Use commonmodal
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/editor-page/app-bar/help-button/help-modal.tsx
+++ src/components/editor-page/app-bar/help-button/help-modal.tsx
@@ -5,10 +5,9 @@
  */
 
 import { Button, Modal } from 'react-bootstrap'
-import { ForkAwesomeIcon } from '../../../common/fork-awesome/fork-awesome-icon'
-import { Trans } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import React, { useMemo, useState } from 'react'
-import { CommonModalProps } from '../../../common/modals/common-modal'
+import { CommonModal } from '../../../common/modals/common-modal'
 import { Shortcut } from './shortcuts'
 import { Links } from './links'
 import { Cheatsheet } from './cheatsheet'
@@ -19,8 +18,14 @@ export enum HelpTabStatus {
   Links = 'links.title'
 }
 
-export const HelpModal: React.FC<CommonModalProps> = ({ show, onHide }) => {
+export interface HelpModalProps {
+  show: boolean,
+  onHide: () => void
+}
+
+export const HelpModal: React.FC<HelpModalProps> = ({ show, onHide }) => {
   const [tab, setTab] = useState<HelpTabStatus>(HelpTabStatus.Cheatsheet)
+  const { t } = useTranslation()
 
   const tabContent = useMemo(() => {
     switch (tab) {
@@ -33,32 +38,32 @@ export const HelpModal: React.FC<CommonModalProps> = ({ show, onHide }) => {
     }
   }, [tab])
 
-  return <Modal show={ show } onHide={ onHide } animation={ true } className='text-dark' size='lg'>
-    <Modal.Header closeButton>
-      <Modal.Title>
-        <ForkAwesomeIcon icon='question-circle'/> <Trans i18nKey={ 'editor.documentBar.help' }/> – <Trans
-        i18nKey={ `editor.help.${ tab }` }/>
-      </Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <nav className='nav nav-tabs'>
-        <Button variant={ 'light' }
-                className={ `nav-link nav-item ${ tab === HelpTabStatus.Cheatsheet ? 'active' : '' }` }
-                onClick={ () => setTab(HelpTabStatus.Cheatsheet) }>
-          <Trans i18nKey={ 'editor.help.cheatsheet.title' }/>
-        </Button>
-        <Button variant={ 'light' }
-                className={ `nav-link nav-item ${ tab === HelpTabStatus.Shortcuts ? 'active' : '' }` }
-                onClick={ () => setTab(HelpTabStatus.Shortcuts) }>
-          <Trans i18nKey={ 'editor.help.shortcuts.title' }/>
-        </Button>
-        <Button variant={ 'light' }
-                className={ `nav-link nav-item ${ tab === HelpTabStatus.Links ? 'active' : '' }` }
-                onClick={ () => setTab(HelpTabStatus.Links) }>
-          <Trans i18nKey={ 'editor.help.links.title' }/>
-        </Button>
-      </nav>
-      { tabContent }
-    </Modal.Body>
-  </Modal>
+  const tabTitle = useMemo(() => t('editor.documentBar.help') + ' - ' + t(`editor.help.${ tab }`), [t, tab])
+
+  return (
+    <CommonModal icon={ 'question-circle' } show={ show } onHide={ onHide } title={ tabTitle }>
+      <Modal.Body>
+        <nav className='nav nav-tabs'>
+          <Button
+            variant={ 'light' }
+            className={ `nav-link nav-item ${ tab === HelpTabStatus.Cheatsheet ? 'active' : '' }` }
+            onClick={ () => setTab(HelpTabStatus.Cheatsheet) }>
+            <Trans i18nKey={ 'editor.help.cheatsheet.title' }/>
+          </Button>
+          <Button
+            variant={ 'light' }
+            className={ `nav-link nav-item ${ tab === HelpTabStatus.Shortcuts ? 'active' : '' }` }
+            onClick={ () => setTab(HelpTabStatus.Shortcuts) }>
+            <Trans i18nKey={ 'editor.help.shortcuts.title' }/>
+          </Button>
+          <Button
+            variant={ 'light' }
+            className={ `nav-link nav-item ${ tab === HelpTabStatus.Links ? 'active' : '' }` }
+            onClick={ () => setTab(HelpTabStatus.Links) }>
+            <Trans i18nKey={ 'editor.help.links.title' }/>
+          </Button>
+        </nav>
+        { tabContent }
+      </Modal.Body>
+    </CommonModal>)
 }
