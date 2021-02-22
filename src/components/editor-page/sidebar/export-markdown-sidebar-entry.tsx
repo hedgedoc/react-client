99bb18cfed6ccb110befb71b863e33df4ec4aef6
Erik Michelson <github@erik.michelson.eu>
2021-02-22 01:55:23 +0100

    Set sanitized note title as filename for download
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

--- src/components/editor-page/sidebar/export-markdown-sidebar-entry.tsx
+++ src/components/editor-page/sidebar/export-markdown-sidebar-entry.tsx
@@ -5,18 +5,22 @@
  */
 
 import React, { useCallback } from 'react'
+import sanitize from 'sanitize-filename'
 import { Trans, useTranslation } from 'react-i18next'
 import { useNoteMarkdownContent } from '../../../hooks/common/use-note-markdown-content'
 import { download } from '../../common/download/download'
 import { SidebarButton } from './sidebar-button'
+import { useSelector } from 'react-redux'
+import { ApplicationState } from '../../../redux'
 
 export const ExportMarkdownSidebarEntry: React.FC = () => {
-  useTranslation()
-
+  const { t } = useTranslation()
+  const noteTitle = useSelector((state: ApplicationState) => state.noteDetails.noteTitle)
   const markdownContent = useNoteMarkdownContent()
   const onClick = useCallback(() => {
-    download(markdownContent, `title.md`, 'text/markdown') //todo: replace hard coded title with redux
-  }, [markdownContent])
+    const sanitized = sanitize(noteTitle)
+    download(markdownContent, `${ sanitized !== '' ? sanitized : t('editor.untitledNote') }.md`, 'text/markdown')
+  }, [markdownContent, noteTitle, t])
 
   return (
     <SidebarButton data-cy={ 'menu-export-markdown' } onClick={ onClick } icon={ 'file-text' }>
