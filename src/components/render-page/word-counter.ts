adeab658985b09b362a245b2c828e09b389241b5
Erik Michelson <github@erik.michelson.eu>
2021-02-28 03:32:08 +0100

    Add word count logic in iframe
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

---
+++ src/components/render-page/word-counter.ts
@@ -0,0 +1,25 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import wordsCount from 'words-count'
+
+const EXCLUDED_TAGS = ['img', 'pre', 'nav']
+
+export const countWordsRecursive = (node: Element | ChildNode): number => {
+  let words = 0
+  if (!node || EXCLUDED_TAGS.includes(node.nodeName.toLowerCase()) ||
+    (node as HTMLElement)?.classList.contains('katex-mathml')) {
+    return 0
+  }
+  if (node.hasChildNodes()) {
+    node.childNodes.forEach(childNode => {
+      words += countWordsRecursive(childNode)
+    })
+  } else {
+    return wordsCount(node.textContent ?? '')
+  }
+  return words
+}
