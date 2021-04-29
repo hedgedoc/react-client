0a35bbc98208d7dba8bc00d6be868a7325a0b194
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-04-29 09:38:59 +0200

    Fix render origin in test
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- cypress/support/config.ts
+++ cypress/support/config.ts
@@ -57,8 +57,8 @@ export const config = {
     issueTrackerUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
   },
   'iframeCommunication': {
-    'editorOrigin': 'http://127.0.0.1:3001',
-    'rendererOrigin': 'http://127.0.0.1:3001'
+    'editorOrigin': 'http://127.0.0.1:3001/',
+    'rendererOrigin': 'http://127.0.0.1:3001/'
   }
 }
 
