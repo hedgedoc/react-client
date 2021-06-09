fc6929b744d3bfae11674a24c90c4ba89818debf
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-06-09 19:02:39 +0200

    Add docs
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/hooks/common/use-application-state.ts
+++ src/hooks/common/use-application-state.ts
@@ -8,9 +8,15 @@ import { useSelector } from 'react-redux'
 import { ApplicationState } from '../../redux'
 import equal from 'fast-deep-equal'
 
+/**
+ * Accesses the global application state to retrieve information.
+ *
+ * @param selector A selector function that extracts the needed information from the state.
+ * @param checkForEquality An optional custom equality function. If not provided then {@link equal equal from fast-deep-equal} will be used.
+ */
 export const useApplicationState = <TSelected>(
   selector: (state: ApplicationState) => TSelected,
-  equalityFn?: (a: TSelected, b: TSelected) => boolean
+  checkForEquality?: (a: TSelected, b: TSelected) => boolean
 ): TSelected => {
-  return useSelector<ApplicationState, TSelected>(selector, equalityFn ? equalityFn : equal)
+  return useSelector<ApplicationState, TSelected>(selector, checkForEquality ? checkForEquality : equal)
 }
