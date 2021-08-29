37a03cf9a1639b27c91602a0f077caccfd856d58
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-08-29 22:42:02 +0200

    Fix DoS in CSV parser
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/components/markdown-renderer/replace-components/csv/csv-parser.ts
+++ src/components/markdown-renderer/replace-components/csv/csv-parser.ts
@@ -4,11 +4,28 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+/**
+ * Parses a given text and parses it as comma separated values (CSV)
+ *
+ * @param csvText The raw csv text
+ * @param csvColumnDelimiter The delimiter for the columns
+ * @return the values splitted by rows and columns
+ */
 export const parseCsv = (csvText: string, csvColumnDelimiter: string): string[][] => {
   const rows = csvText.split('\n')
   if (!rows || rows.length === 0) {
     return []
   }
-  const splitRegex = new RegExp(`${csvColumnDelimiter}(?=(?:[^"]*"[^"]*")*[^"]*$)`)
-  return rows.filter((row) => row !== '').map((row) => row.split(splitRegex))
+  const splitRegex = new RegExp(`${escapeRegexCharacters(csvColumnDelimiter)}(?=(?:[^"]*"[^"]*")*[^"]*$)`)
+  return rows.filter((row) => row !== '')
+             .map((row) => row.split(splitRegex))
+}
+
+/**
+ * Escapes regex characters in the given string so it can be used as literal string in another regex.
+ * @param unsafe The unescaped string
+ * @return The escaped string
+ */
+function escapeRegexCharacters(unsafe: string) {
+  return unsafe.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 }
