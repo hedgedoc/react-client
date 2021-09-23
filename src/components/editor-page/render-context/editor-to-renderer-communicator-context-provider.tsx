0d9ffafc72d3a27716787b2dea0407f4e3565d22
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-09-23 22:12:41 +0200

    Fix typo
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/components/editor-page/render-context/editor-to-renderer-communicator-context-provider.tsx
+++ src/components/editor-page/render-context/editor-to-renderer-communicator-context-provider.tsx
@@ -7,7 +7,7 @@
 import React, { createContext, useContext, useMemo } from 'react'
 import { EditorToRendererCommunicator } from '../../render-page/window-post-message-communicator/editor-to-renderer-communicator'
 
-const editorToRendererCommunicatorContext = createContext<EditorToRendererCommunicator | undefined>(undefined)
+const EditorToRendererCommunicatorContext = createContext<EditorToRendererCommunicator | undefined>(undefined)
 
 /**
  * Provides the {@link EditorToRendererCommunicator editor to renderer iframe communicator} that is set by a {@link EditorToRendererCommunicatorContextProvider context provider}.
@@ -16,7 +16,7 @@ const editorToRendererCommunicatorContext = createContext<EditorToRendererCommun
  * @throws Error if no communicator was received
  */
 export const useEditorToRendererCommunicator: () => EditorToRendererCommunicator = () => {
-  const communicatorFromContext = useContext(editorToRendererCommunicatorContext)
+  const communicatorFromContext = useContext(EditorToRendererCommunicatorContext)
   if (!communicatorFromContext) {
     throw new Error('No editor-to-renderer-iframe-communicator received. Did you forget to use the provider component?')
   }
@@ -27,8 +27,8 @@ export const EditorToRendererCommunicatorContextProvider: React.FC = ({ children
   const communicator = useMemo<EditorToRendererCommunicator>(() => new EditorToRendererCommunicator(), [])
 
   return (
-    <editorToRendererCommunicatorContext.Provider value={communicator}>
+    <EditorToRendererCommunicatorContext.Provider value={communicator}>
       {children}
-    </editorToRendererCommunicatorContext.Provider>
+    </EditorToRendererCommunicatorContext.Provider>
   )
 }
