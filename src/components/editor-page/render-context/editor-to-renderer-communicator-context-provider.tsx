236fe57245bf380e96dc4996dd16165b1bca04c1
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-09-23 22:01:06 +0200

    Rename var
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/components/editor-page/render-context/editor-to-renderer-communicator-context-provider.tsx
+++ src/components/editor-page/render-context/editor-to-renderer-communicator-context-provider.tsx
@@ -7,7 +7,7 @@
 import React, { createContext, useContext, useMemo } from 'react'
 import { EditorToRendererCommunicator } from '../../render-page/window-post-message-communicator/editor-to-renderer-communicator'
 
-const IFrameEditorToRendererCommunicatorContext = createContext<EditorToRendererCommunicator | undefined>(undefined)
+const editorToRendererCommunicatorContext = createContext<EditorToRendererCommunicator | undefined>(undefined)
 
 /**
  * Provides the {@link EditorToRendererCommunicator editor to renderer iframe communicator} that is set by a {@link EditorToRendererCommunicatorContextProvider context provider}.
@@ -16,7 +16,7 @@ const IFrameEditorToRendererCommunicatorContext = createContext<EditorToRenderer
  * @throws Error if no communicator was received
  */
 export const useEditorToRendererCommunicator: () => EditorToRendererCommunicator = () => {
-  const communicatorFromContext = useContext(IFrameEditorToRendererCommunicatorContext)
+  const communicatorFromContext = useContext(editorToRendererCommunicatorContext)
   if (!communicatorFromContext) {
     throw new Error('No editor-to-renderer-iframe-communicator received. Did you forget to use the provider component?')
   }
@@ -27,8 +27,8 @@ export const EditorToRendererCommunicatorContextProvider: React.FC = ({ children
   const communicator = useMemo<EditorToRendererCommunicator>(() => new EditorToRendererCommunicator(), [])
 
   return (
-    <IFrameEditorToRendererCommunicatorContext.Provider value={communicator}>
+    <editorToRendererCommunicatorContext.Provider value={communicator}>
       {children}
-    </IFrameEditorToRendererCommunicatorContext.Provider>
+    </editorToRendererCommunicatorContext.Provider>
   )
 }
