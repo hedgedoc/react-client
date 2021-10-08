57923d4a59a48728c29ff828cb5b7b9469f9cddd
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-10-09 00:10:26 +0200

    Refactor iframe load callback to fix race condition
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

---
+++ src/components/editor-page/renderer-pane/hooks/use-force-url-on-iframe-load-callback.ts
@@ -0,0 +1,50 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { RefObject, useCallback, useEffect, useRef } from 'react'
+import { Logger } from '../../../../utils/logger'
+
+const log = new Logger('IframeLoader')
+
+/**
+ * Generates a callback for a iframe load handler, that enforces a given URL if frame navigates away.
+ *
+ * @param iFrameReference A reference to the iframe react dom element.
+ * @param forcedUrl The url that should be enforced.
+ * @param onNavigateAway An optional callback that is executed when the iframe leaves the enforced URL.
+ */
+export const useForceUrlOnIframeLoadCallback = (
+  iFrameReference: RefObject<HTMLIFrameElement>,
+  forcedUrl: string,
+  onNavigateAway?: () => void
+): (() => void) => {
+  const redirectionInProgress = useRef<boolean>(false)
+
+  const loadCallback = useCallback(() => {
+    const frame = iFrameReference.current
+
+    if (!frame) {
+      log.debug('No frame in reference')
+      return
+    }
+
+    if (redirectionInProgress.current) {
+      redirectionInProgress.current = false
+      log.debug('Redirect complete')
+    } else {
+      log.warn(`Navigated away from unknown URL. Forcing back to ${forcedUrl}`)
+      onNavigateAway?.()
+      redirectionInProgress.current = true
+      frame.src = forcedUrl
+    }
+  }, [iFrameReference, onNavigateAway, forcedUrl])
+
+  useEffect(() => {
+    loadCallback()
+  }, [loadCallback])
+
+  return loadCallback
+}
