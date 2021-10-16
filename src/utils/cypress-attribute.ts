580fa39f012461cfe3844688105bee1074b48f75
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-10-16 23:06:24 +0200

    Add function for test attribute
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

---
+++ src/utils/cypress-attribute.ts
@@ -0,0 +1,31 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { isTestMode } from './test-modes'
+
+interface PropsWithDataCy {
+  'data-cy'?: string | undefined
+}
+
+/**
+ * Returns an object with the "data-cy" attribute that is used to find
+ * elements for integration tests.
+ * This works only if the runtime is built in test mode.
+ *
+ * @param identifier The identifier that is used to find the element
+ * @return An object if in test mode, undefined otherwise.
+ */
+export const dataCy = (identifier: string | undefined | PropsWithDataCy): Record<'dataCy', string> | undefined => {
+  if (!isTestMode() || !identifier) {
+    return
+  }
+
+  const attributeContent = typeof identifier === 'string' ? identifier : identifier['data-cy']
+
+  if (attributeContent !== undefined) {
+    return { dataCy: attributeContent }
+  }
+}
