591ba1fea44284fd0f92d06ee75ddc2ab2247a5c
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-11-27 10:34:46 +0100

    Reformat code
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/components/editor-page/editor-pane/tool-bar/table-picker/custom-table-size-modal.tsx
+++ src/components/editor-page/editor-pane/tool-bar/table-picker/custom-table-size-modal.tsx
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import type { ChangeEvent} from 'react';
+import type { ChangeEvent } from 'react'
 import React, { useCallback, useEffect, useState } from 'react'
 import { Button, Form, ModalFooter } from 'react-bootstrap'
 import { Trans, useTranslation } from 'react-i18next'
