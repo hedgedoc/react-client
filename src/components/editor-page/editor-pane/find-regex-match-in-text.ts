bdbcdd20afcfd466ec372b0da59a856fa8e5be31
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-12-09 23:08:47 +0100

    fix docs
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/components/editor-page/editor-pane/find-regex-match-in-text.ts
+++ src/components/editor-page/editor-pane/find-regex-match-in-text.ts
@@ -5,19 +5,19 @@
  */
 
 /**
- * Matches a regex against a given line and returns the n-th match of the regex.
+ * Matches a regex against a given text and returns the n-th match of the regex.
  *
- * @param line The line that should be searched through
- * @param regex The regex that should find matches in the line
+ * @param text The text that should be searched through
+ * @param regex The regex that should find matches in the text
  * @param matchIndex The index of the match to find
  * @return The regex match of the found occurrence or undefined if no match could be found
  */
-export const findRegexMatchInText = (line: string, regex: RegExp, matchIndex: number): RegExpMatchArray | undefined => {
+export const findRegexMatchInText = (text: string, regex: RegExp, matchIndex: number): RegExpMatchArray | undefined => {
   if (matchIndex < 0) {
     return
   }
   let currentIndex = 0
-  for (const match of line.matchAll(regex)) {
+  for (const match of text.matchAll(regex)) {
     if (currentIndex === matchIndex) {
       return match
     }
