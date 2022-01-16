4347df519b7cae9cb17de95a9f288c8c4c25fc97
Erik Michelson <github@erik.michelson.eu>
2022-01-16 14:16:08 +0100

    Add basic abcjs midi player
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

--- src/components/markdown-renderer/markdown-extension/abcjs/abc-frame.tsx
+++ src/components/markdown-renderer/markdown-extension/abcjs/abc-frame.tsx
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import React, { useEffect, useRef } from 'react'
+import React, { Fragment, useEffect, useMemo, useRef } from 'react'
 import styles from './abc.module.scss'
 import { Logger } from '../../../../utils/logger'
 import type { CodeProps } from '../../replace-components/code-block-component-replacer'
@@ -13,16 +13,38 @@ import { cypressId } from '../../../../utils/cypress-attribute'
 const log = new Logger('AbcFrame')
 
 export const AbcFrame: React.FC<CodeProps> = ({ code }) => {
-  const container = useRef<HTMLDivElement>(null)
+  const scoreContainerRef = useRef<HTMLDivElement>(null)
+
+  const randomId = useMemo(() => Math.random().toString(36).slice(7), [])
 
   useEffect(() => {
-    if (!container.current) {
+    if (!scoreContainerRef.current) {
       return
     }
-    const actualContainer = container.current
+    const scoreContainer = scoreContainerRef.current
     import(/* webpackChunkName: "abc.js" */ 'abcjs')
       .then((importedLibrary) => {
-        importedLibrary.renderAbc(actualContainer, code, {})
+        const rendering = importedLibrary.renderAbc(scoreContainer, code, {})
+        if (importedLibrary.synth.supportsAudio()) {
+          const synthControls = new importedLibrary.synth.SynthController()
+          synthControls.load(`#abcjs-player-${randomId}`, undefined, {
+            displayPlay: true,
+            displayProgress: true,
+            displayLoop: true
+          })
+          const synthModule = new importedLibrary.synth.CreateSynth()
+          synthModule
+            .init({
+              visualObj: rendering[0]
+            })
+            .then(() => synthControls.setTune(rendering[0], false))
+            .then(() => {
+              log.debug('abcjs audio initialized')
+            })
+            .catch((error) => {
+              log.error('abcjs audio initialization failed:', error)
+            })
+        }
       })
       .catch((error: Error) => {
         log.error('Error while loading abcjs', error)
@@ -30,10 +52,13 @@ export const AbcFrame: React.FC<CodeProps> = ({ code }) => {
   }, [code])
 
   return (
-    <div
-      ref={container}
-      className={`${styles['abcjs-score']} bg-white text-black svg-container`}
-      {...cypressId('abcjs')}
-    />
+    <Fragment>
+      <div
+        ref={scoreContainerRef}
+        className={`${styles['abcjs-score']} bg-white text-black svg-container`}
+        {...cypressId('abcjs')}
+      />
+      <div id={`abcjs-player-${randomId}`}>&nbsp;</div>
+    </Fragment>
   )
 }
