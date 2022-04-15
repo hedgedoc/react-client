5b0d4854de844aa48cafc826d58a20c16f9033af
Tilman Vatteroth <git@tilmanvatteroth.de>
2022-04-15 10:39:02 +0200

    Fix props
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/components/editor-page/document-bar/note-info/unitalic-bold-content.tsx
+++ src/components/editor-page/document-bar/note-info/unitalic-bold-content.tsx
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+import type { PropsWithChildren } from 'react'
 import React from 'react'
 import { cypressId } from '../../../../utils/cypress-attribute'
 
@@ -15,7 +16,7 @@ export interface UnitalicBoldContentProps {
  * Renders the children elements in a non-italic but bold style.
  * @param text Optional text content that should be rendered.
  */
-export const UnitalicBoldContent: React.FC<UnitalicBoldContentProps> = ({ text, children, ...props }) => {
+export const UnitalicBoldContent: React.FC<PropsWithChildren<UnitalicBoldContentProps>> = ({ text, children, ...props }) => {
   return (
     <strong className={'font-style-normal mr-1'} {...cypressId(props)}>
       {text}
