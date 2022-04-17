f4f09a95ffd27865d8b87b278776ae9e236004fe
Erik Michelson <github@erik.michelson.eu>
2022-04-18 01:28:30 +0200

    Add new note page
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

---
+++ src/pages/new.tsx
@@ -0,0 +1,31 @@
+/*
+ * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import type { NextPage } from 'next'
+import { useSingleStringUrlParameter } from '../hooks/common/use-single-string-url-parameter'
+import { useAsync } from 'react-use'
+import { createNote } from '../api/notes'
+import { AsyncLoadingBoundary } from '../components/common/async-loading-boundary'
+import { Redirect } from '../components/common/redirect'
+
+/**
+ * Creates a new note, optionally including the passed content and redirects to that note.
+ */
+export const NewNotePage: NextPage = () => {
+  const newContent = useSingleStringUrlParameter('content', '')
+
+  const { loading, error, value } = useAsync(() => {
+    return createNote(newContent)
+  }, [newContent])
+
+  return (
+    <AsyncLoadingBoundary loading={loading} componentName={'NewNotePage'} error={error}>
+      {value ? <Redirect to={`/n/${value.metadata.primaryAddress}`} /> : null}
+    </AsyncLoadingBoundary>
+  )
+}
+
+export default NewNotePage
