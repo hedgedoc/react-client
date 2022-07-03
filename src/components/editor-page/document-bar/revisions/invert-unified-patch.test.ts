144f6948b9979f46303a9b98cdd8652a9c50b890
Tilman Vatteroth <git@tilmanvatteroth.de>
2022-07-03 21:50:33 +0200

    Fix test
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/components/editor-page/document-bar/revisions/invert-unified-patch.test.ts
+++ src/components/editor-page/document-bar/revisions/invert-unified-patch.test.ts
@@ -5,10 +5,11 @@
  */
 
 import { invertUnifiedPatch } from './invert-unified-patch'
+import { parsePatch } from 'diff'
 
 describe('invert unified patch', () => {
   it('inverts a patch correctly', () => {
-    const result = invertUnifiedPatch(`--- a\t2022-07-03 21:21:07.499933337 +0200
+    const parsedPatch = parsePatch(`--- a\t2022-07-03 21:21:07.499933337 +0200
 +++ b\t2022-07-03 21:22:28.650972217 +0200
 @@ -1,5 +1,4 @@
 -a
@@ -16,7 +17,8 @@ describe('invert unified patch', () => {
  c
  d
 +d
- e`)
+ e`)[0]
+    const result = invertUnifiedPatch(parsedPatch)
     expect(result).toMatchInlineSnapshot(`
       Object {
         "hunks": Array [
