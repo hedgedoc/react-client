f3c1055be3e70c9f6f86e0cd689bb033701c64ab
Tilman Vatteroth <git@tilmanvatteroth.de>
2022-07-31 17:21:56 +0200

    Add docs
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/utils/wait-for-other-promises-to-finish.ts
+++ src/utils/wait-for-other-promises-to-finish.ts
@@ -6,7 +6,12 @@
 
 /**
  * Waits until all other pending promises are processed.
+ *
+ * NodeJS has a queue for async code that waits for being processed. This method adds a promise to the very end of this queue.
+ * If the promise is resolved then this means that all other promises before it have been processed as well.
+ *
+ * @return A promise which resolves when all other promises have been processed
  */
-export async function waitForOtherPromisesToFinish(): Promise<void> {
-  return await new Promise((resolve) => process.nextTick(resolve))
+export function waitForOtherPromisesToFinish(): Promise<void> {
+  return new Promise((resolve) => process.nextTick(resolve))
 }
