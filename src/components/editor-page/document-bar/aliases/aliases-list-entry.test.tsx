307576b81372f2dc304091d25bbb21981296f409
Erik Michelson <github@erik.michelson.eu>
2022-09-18 22:38:12 +0200

    Add tests
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

---
+++ src/components/editor-page/document-bar/aliases/aliases-list-entry.test.tsx
@@ -0,0 +1,74 @@
+/*
+ * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { render, act, screen } from '@testing-library/react'
+import React from 'react'
+import { mockI18n } from '../../../markdown-renderer/test-utils/mock-i18n'
+import type { Alias } from '../../../../api/alias/types'
+import { AliasesListEntry } from './aliases-list-entry'
+import * as AliasModule from '../../../../api/alias'
+import * as NoteDetailsReduxModule from '../../../../redux/note-details/methods'
+
+jest.mock('../../../../api/alias')
+jest.mock('../../../../redux/note-details/methods')
+
+const deletePromise = Promise.resolve()
+const markAsPrimaryPromise = Promise.resolve({ name: 'mock', primaryAlias: true, noteId: 'mock' })
+
+describe('AliasesListEntry', () => {
+  beforeEach(async () => {
+    await mockI18n()
+    jest.spyOn(AliasModule, 'deleteAlias').mockImplementation(() => deletePromise)
+    jest.spyOn(AliasModule, 'markAliasAsPrimary').mockImplementation(() => markAsPrimaryPromise)
+    jest.spyOn(NoteDetailsReduxModule, 'updateMetadata').mockImplementation(() => Promise.resolve())
+  })
+
+  afterAll(() => {
+    jest.resetAllMocks()
+    jest.resetModules()
+  })
+
+  it('renders an AliasesListEntry that is primary', async () => {
+    const testAlias: Alias = {
+      name: 'test-primary',
+      primaryAlias: true,
+      noteId: 'test-note-id'
+    }
+    const view = render(<AliasesListEntry alias={testAlias} />)
+    expect(view.container).toMatchSnapshot()
+    const button = screen.getByTitle('editor.modal.aliases.removeAlias')
+    act(() => {
+      button.click()
+    })
+    expect(AliasModule.deleteAlias).toBeCalledWith(testAlias.name)
+    await deletePromise
+    expect(NoteDetailsReduxModule.updateMetadata).toBeCalled()
+  })
+
+  it('renders an AliasesListEntry that is not primary', async () => {
+    const testAlias: Alias = {
+      name: 'test-non-primary',
+      primaryAlias: false,
+      noteId: 'test-note-id'
+    }
+    const view = render(<AliasesListEntry alias={testAlias} />)
+    expect(view.container).toMatchSnapshot()
+    const buttonRemove = screen.getByTitle('editor.modal.aliases.removeAlias')
+    act(() => {
+      buttonRemove.click()
+    })
+    expect(AliasModule.deleteAlias).toBeCalledWith(testAlias.name)
+    await deletePromise
+    expect(NoteDetailsReduxModule.updateMetadata).toBeCalled()
+    const buttonMakePrimary = screen.getByTitle('editor.modal.aliases.makePrimary')
+    act(() => {
+      buttonMakePrimary.click()
+    })
+    expect(AliasModule.markAliasAsPrimary).toBeCalledWith(testAlias.name)
+    await markAsPrimaryPromise
+    expect(NoteDetailsReduxModule.updateMetadata).toBeCalled()
+  })
+})
